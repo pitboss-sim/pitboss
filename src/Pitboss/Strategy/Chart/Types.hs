3d292f91b7b068f0990c9ba00f05e32ec657e465
Pitboss <pitboss.sim@gmail.com>
2025-05-02 04:09:00 -0700

    basic strategy parsing

---
+++ src/Pitboss/Strategy/Chart/Types.hs
@@ -0,0 +1,31 @@
+module Pitboss.Strategy.Chart.Types where
+
+import Data.Map.Strict
+import Pitboss.Blackjack.Card (Rank)
+
+data HandPrefix
+  = PA
+  | PT
+  | P Int
+  | A Int
+  | H Int
+  deriving (Eq, Ord, Show)
+
+data MoveCode
+  = MoveHit
+  | MoveStand
+  | MoveDoubleOrHit
+  | MoveDoubleOrStand
+  | MoveSplit
+  | MoveSplitOrHit
+  | MoveSurrenderOrStand
+  | MoveUndefined
+  deriving (Eq, Show)
+
+data ChartEntry = ChartEntry
+  { handPrefix :: HandPrefix,
+    moves :: Map Rank MoveCode
+  }
+  deriving (Eq, Show)
+
+type StrategyChart = [ChartEntry]

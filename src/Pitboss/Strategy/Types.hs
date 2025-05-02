2c20b15dd3ad81224a16a352a5b7554065d90820
Pitboss <pitboss.sim@gmail.com>
2025-05-02 04:09:00 -0700

    rudimentary strategy

---
+++ src/Pitboss/Strategy/Types.hs
@@ -0,0 +1,18 @@
+module Pitboss.Strategy.Types where
+
+newtype Fallback
+    = Else Move
+    deriving (Eq, Show)
+
+data Decision
+    = Always Move
+    | Prefer Move Fallback
+    deriving (Eq, Show)
+
+data Move
+    = Hit
+    | Stand
+    | Double
+    | Split
+    | Surrender
+    deriving (Eq, Show)

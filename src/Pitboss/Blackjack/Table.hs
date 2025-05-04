c70e30e31a48854845299c469a72091fad181c1e
Pitboss <pitboss.sim@gmail.com>
2025-05-03 17:51:25 -0700

    fixup platonics

--- src/Pitboss/Blackjack/Table.hs
+++ src/Pitboss/Blackjack/Table.hs
@@ -1,10 +1,16 @@
 module Pitboss.Blackjack.Table
   ( allSeats,
     Table (..),
+    TableSeatIx,
   )
 where
 
 import Pitboss.Blackjack.Types.Index (SeatIx)
+
+data TableSeatIx = Seat1 | Seat2 | Seat3 | Seat4 | Seat5 | Seat6
+  deriving (Eq, Ord, Enum, Bounded, Show)
+
+instance BoundedEnum SeatIx
 
 allSeats :: [SeatIx]
 allSeats = [minBound .. maxBound]

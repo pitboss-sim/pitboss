bbf0fd43206fa021af774ecde107cd1a1edb780c
Pitboss <pitboss.sim@gmail.com>
2025-05-05 02:52:00 -0700

    fixup sim state

--- src/Pitboss/Sim/State/Spot/Mutation.hs
+++ src/Pitboss/Sim/State/Spot/Mutation.hs
@@ -2,9 +2,14 @@
 
 module Pitboss.Sim.State.Spot.Mutation where
 
-import Pitboss.Sim.State.Spot (SpotState (..), Turn (..))
-import Pitboss.Sim.Types.FiniteMap (lookupFiniteMap)
-import Pitboss.Sim.Types.Occupancy (Occupancy (..))
+import Control.Lens hiding (universe)
+import Pitboss.Blackjack.Card (Card)
+import Pitboss.Blackjack.Hand (Hand (..))
+import Pitboss.Sim.State.Spot (SpotHandIx, SpotState (..), Turn (..))
+import Pitboss.Sim.State.Spot.Lens (lensHands)
+import Pitboss.Sim.State.SpotHand (SpotHandPlayState (..), SpotHandState (..))
+import Pitboss.Sim.Types.FiniteMap (insertFiniteMap, lookupFiniteMap, toListFiniteMap)
+import Pitboss.Sim.Types.Occupancy (Occupancy (..), isAbsent)
 import Pitboss.Types.BoundedEnum (universe)
 
 advanceToNextHand :: SpotState -> SpotState
@@ -20,3 +25,24 @@ advanceToNextHand spot@(SpotState handsMap currentTurn _) =
            in case remaining of
                 (_ : next : _) -> spot {turn = Playing next}
                 _ -> spot {turn = NoHandSelected}
+
+nextFreeHands :: SpotState -> [SpotHandIx]
+nextFreeHands spot =
+  [hid | (hid, occ) <- toListFiniteMap (spot ^. lensHands), isAbsent occ]
+
+addCardsToSplitPlayHands ::
+  Card -> Card -> Card -> Card -> SpotHandIx -> SpotHandIx -> SpotState -> SpotState
+addCardsToSplitPlayHands orig1 new1 orig2 new2 h1 h2 =
+  over lensHands $
+    insertFiniteMap h2 (Present (SpotHandState NormalPlay (Hand [orig2, new2])))
+      . insertFiniteMap h1 (Present (SpotHandState NormalPlay (Hand [orig1, new1])))
+
+nextAvailableSpotHand :: SpotState -> Maybe SpotHandIx
+nextAvailableSpotHand spot =
+  let isLive (_, Present (SpotHandState ps _)) = ps == NormalPlay
+      isLive _ = False
+   in fmap fst . safeHead . filter isLive . toListFiniteMap $ spot ^. lensHands
+
+safeHead :: [a] -> Maybe a
+safeHead [] = Nothing
+safeHead (x : _) = Just x
--- src/Pitboss/Sim/State/SpotHand/Mutation.hs
+++ src/Pitboss/Sim/State/SpotHand/Mutation.hs
@@ -1,13 +1,11 @@
 module Pitboss.Sim.State.SpotHand.Mutation where
 
 import Control.Lens
-import Pitboss.Blackjack.Card (Card)
-import Pitboss.Blackjack.Hand (Hand (..))
 import Pitboss.Sim.State.Spot
 import Pitboss.Sim.State.Spot.Lens (lensHands)
 import Pitboss.Sim.State.SpotHand (SpotHandPlayState (..), SpotHandState (..))
 import Pitboss.Sim.Types.FiniteMap
-import Pitboss.Sim.Types.Occupancy (Occupancy (..), isAbsent)
+import Pitboss.Sim.Types.Occupancy (Occupancy (..))
 
 freezeOnly :: SpotHandIx -> SpotHandIx -> Occupancy SpotHandState -> Occupancy SpotHandState
 freezeOnly target current (Present (SpotHandState NormalPlay h))
@@ -17,24 +15,3 @@ freezeOnly _ _ occ = occ
 freezeSelectedPlayHand :: SpotHandIx -> SpotState -> SpotState
 freezeSelectedPlayHand target spot =
   spot & lensHands %~ mapFiniteMapWithKey (freezeOnly target)
-
-nextFreeHands :: SpotState -> [SpotHandIx]
-nextFreeHands spot =
-  [hid | (hid, occ) <- toListFiniteMap (spot ^. lensHands), isAbsent occ]
-
-addCardsToSplitPlayHands ::
-  Card -> Card -> Card -> Card -> SpotHandIx -> SpotHandIx -> SpotState -> SpotState
-addCardsToSplitPlayHands orig1 new1 orig2 new2 h1 h2 =
-  over lensHands $
-    insertFiniteMap h2 (Present (SpotHandState NormalPlay (Hand [orig2, new2])))
-      . insertFiniteMap h1 (Present (SpotHandState NormalPlay (Hand [orig1, new1])))
-
-nextAvailableSpotHand :: SpotState -> Maybe SpotHandIx
-nextAvailableSpotHand spot =
-  let isLive (_, Present (SpotHandState ps _)) = ps == NormalPlay
-      isLive _ = False
-   in fmap fst . safeHead . filter isLive . toListFiniteMap $ spot ^. lensHands
-
-safeHead :: [a] -> Maybe a
-safeHead [] = Nothing
-safeHead (x : _) = Just x

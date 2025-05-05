7867b54df3d60cc1f89bd37e047ac049f7d66186
Pitboss <pitboss.sim@gmail.com>
2025-05-05 01:40:28 -0700

    poc: finite state machines

---
+++ src/Pitboss/Blackjack/FSM/Game/ENHC.hs
@@ -0,0 +1,48 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE StandaloneDeriving #-}
+
+module Pitboss.Blackjack.FSM.Game.ENHC where
+
+data ENHCPhase
+  = ENHCAwaiting
+  | ENHCBets
+  | ENHCDeal
+  | ENHCPlayers
+  | ENHCDealer
+  | ENHCSettle
+  | ENHCComplete
+
+data ENHCFSM (p :: ENHCPhase) where
+  ENHCAwaitingFSM :: ENHCFSM 'ENHCAwaiting
+  ENHCBetsFSM :: ENHCFSM 'ENHCBets
+  ENHCDealFSM :: ENHCFSM 'ENHCDeal
+  ENHCPlayersFSM :: ENHCFSM 'ENHCPlayers
+  ENHCDealerFSM :: ENHCFSM 'ENHCDealer
+  ENHCSettleFSM :: ENHCFSM 'ENHCSettle
+  ENHCCompleteFSM :: ENHCFSM 'ENHCComplete
+
+deriving instance Show (ENHCFSM p)
+
+deriving instance Eq (ENHCFSM p)
+
+-- advancement
+
+beginENHC :: ENHCFSM 'ENHCAwaiting -> ENHCFSM 'ENHCBets
+beginENHC ENHCAwaitingFSM = ENHCBetsFSM
+
+betsPlacedENHC :: ENHCFSM 'ENHCBets -> ENHCFSM 'ENHCDeal
+betsPlacedENHC ENHCBetsFSM = ENHCDealFSM
+
+dealCardsENHC :: ENHCFSM 'ENHCDeal -> ENHCFSM 'ENHCPlayers
+dealCardsENHC ENHCDealFSM = ENHCPlayersFSM
+
+finishPlayersENHC :: ENHCFSM 'ENHCPlayers -> ENHCFSM 'ENHCDealer
+finishPlayersENHC ENHCPlayersFSM = ENHCDealerFSM
+
+finishDealerENHC :: ENHCFSM 'ENHCDealer -> ENHCFSM 'ENHCSettle
+finishDealerENHC ENHCDealerFSM = ENHCSettleFSM
+
+resolvePayoutsENHC :: ENHCFSM 'ENHCSettle -> ENHCFSM 'ENHCComplete
+resolvePayoutsENHC ENHCSettleFSM = ENHCCompleteFSM

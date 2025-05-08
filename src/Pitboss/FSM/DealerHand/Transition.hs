050f236984aeb2ed2fc9fa18c7901cbc3418b186
Pitboss <pitboss.sim@gmail.com>
2025-05-08 02:00:36 -0700

    mechanics: finite state machines

---
+++ src/Pitboss/FSM/DealerHand/Transition.hs
@@ -0,0 +1,20 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE GADTs #-}
+
+module Pitboss.FSM.DealerHand.Transition where
+
+import Pitboss.FSM.DealerHand.FSM
+import Pitboss.FSM.DealerHand.Phase
+import Pitboss.FSM.DealerRound hiding (Dealing, Interrupted)
+
+beginEvaluation :: DealerHandFSM 'Dealing -> DealerHandFSM 'Evaluating
+beginEvaluation DealingFSM = EvaluatingFSM
+
+resolveHand :: DealerHandResolution -> DealerHandFSM 'Evaluating -> DealerHandFSM ('Resolved res)
+resolveHand res EvaluatingFSM = ResolvedFSM res
+
+interruptHand :: InterruptReason -> DealerHandFSM p -> DealerHandFSM ('Interrupted r)
+interruptHand reason _ = InterruptedFSM reason
+
+resumeFromInterrupt :: DealerHandFSM ('Interrupted r) -> DealerHandFSM 'Dealing
+resumeFromInterrupt (InterruptedFSM _) = DealingFSM
---
+++ src/Pitboss/FSM/DealerRound/Peek.hs
@@ -0,0 +1,120 @@
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+module Pitboss.FSM.DealerRound.Peek (
+    module Pitboss.FSM.DealerRound.Peek.FSM,
+    module Pitboss.FSM.DealerRound.Peek.Phase,
+    module Pitboss.FSM.DealerRound.Peek.Transition,
+    SomePeekFSM (..),
+    mkSomePeekAwaiting,
+    mkSomePeekBets,
+    mkSomePeekDeal,
+    mkSomePeekEarlySurrender,
+    mkSomePeekPeek,
+    mkSomePeekInsuranceDecision,
+    mkSomePeekInsuranceSettled,
+    mkSomePeekPlayers,
+    mkSomePeekDealing,
+    mkSomePeekSettle,
+    mkSomePeekComplete,
+    mkSomePeekInterrupted,
+)
+where
+
+import Data.Aeson
+import Pitboss.FSM.DealerRound.Peek.FSM
+import Pitboss.FSM.DealerRound.Peek.Phase
+import Pitboss.FSM.DealerRound.Peek.Transition
+import Pitboss.FSM.DealerRound.Phase
+import Pitboss.FSM.Types.Transitionable
+
+data SomePeekFSM = forall p. SomePeekFSM (PeekFSM p)
+
+mkSomePeekAwaiting :: SomePeekFSM
+mkSomePeekAwaiting = SomePeekFSM PeekAwaitingFSM
+
+mkSomePeekBets :: SomePeekFSM
+mkSomePeekBets = SomePeekFSM PeekBetsFSM
+
+mkSomePeekDeal :: SomePeekFSM
+mkSomePeekDeal = SomePeekFSM PeekDealFSM
+
+mkSomePeekEarlySurrender :: SomePeekFSM
+mkSomePeekEarlySurrender = SomePeekFSM PeekEarlySurrenderFSM
+
+mkSomePeekPeek :: SomePeekFSM
+mkSomePeekPeek = SomePeekFSM PeekPeekFSM
+
+mkSomePeekInsuranceDecision :: SomePeekFSM
+mkSomePeekInsuranceDecision = SomePeekFSM PeekInsuranceDecisionFSM
+
+mkSomePeekInsuranceSettled :: SomePeekFSM
+mkSomePeekInsuranceSettled = SomePeekFSM PeekInsuranceSettledFSM
+
+mkSomePeekPlayers :: SomePeekFSM
+mkSomePeekPlayers = SomePeekFSM PeekPlayersFSM
+
+mkSomePeekDealing :: SomePeekFSM
+mkSomePeekDealing = SomePeekFSM PeekDealingFSM
+
+mkSomePeekSettle :: SomePeekFSM
+mkSomePeekSettle = SomePeekFSM PeekSettleFSM
+
+mkSomePeekComplete :: SomePeekFSM
+mkSomePeekComplete = SomePeekFSM PeekCompleteFSM
+
+mkSomePeekInterrupted :: InterruptReason -> SomePeekFSM
+mkSomePeekInterrupted r = SomePeekFSM (PeekInterruptedFSM r)
+
+instance Eq SomePeekFSM where
+    SomePeekFSM a == SomePeekFSM b = case (a, b) of
+        (PeekAwaitingFSM, PeekAwaitingFSM) -> True
+        (PeekBetsFSM, PeekBetsFSM) -> True
+        (PeekDealFSM, PeekDealFSM) -> True
+        (PeekEarlySurrenderFSM, PeekEarlySurrenderFSM) -> True
+        (PeekPeekFSM, PeekPeekFSM) -> True
+        (PeekInsuranceDecisionFSM, PeekInsuranceDecisionFSM) -> True
+        (PeekInsuranceSettledFSM, PeekInsuranceSettledFSM) -> True
+        (PeekPlayersFSM, PeekPlayersFSM) -> True
+        (PeekDealingFSM, PeekDealingFSM) -> True
+        (PeekSettleFSM, PeekSettleFSM) -> True
+        (PeekCompleteFSM, PeekCompleteFSM) -> True
+        _ -> False
+
+instance Transitionable SomePeekFSM where
+    transitionType (SomePeekFSM fsm) = transitionType fsm
+
+instance Show SomePeekFSM where
+    show (SomePeekFSM fsm) = show fsm
+
+instance ToJSON SomePeekFSM where
+    toJSON (SomePeekFSM fsm) = case fsm of
+        PeekAwaitingFSM -> object ["tag" .= String "PeekAwaiting"]
+        PeekBetsFSM -> object ["tag" .= String "PeekBets"]
+        PeekDealFSM -> object ["tag" .= String "PeekDeal"]
+        PeekEarlySurrenderFSM -> object ["tag" .= String "PeekEarlySurrender"]
+        PeekPeekFSM -> object ["tag" .= String "PeekPeek"]
+        PeekInsuranceDecisionFSM -> object ["tag" .= String "PeekInsuranceDecision"]
+        PeekInsuranceSettledFSM -> object ["tag" .= String "PeekInsuranceSettled"]
+        PeekPlayersFSM -> object ["tag" .= String "PeekPlayers"]
+        PeekDealingFSM -> object ["tag" .= String "PeekDealing"]
+        PeekSettleFSM -> object ["tag" .= String "PeekSettle"]
+        PeekCompleteFSM -> object ["tag" .= String "PeekComplete"]
+        PeekInterruptedFSM _ -> object ["tag" .= String "PeekComplete"]
+
+instance FromJSON SomePeekFSM where
+    parseJSON = withObject "SomePeekFSM" $ \obj -> do
+        tag <- obj .: "tag"
+        case tag of
+            "PeekAwaiting" -> pure $ SomePeekFSM PeekAwaitingFSM
+            "PeekBets" -> pure $ SomePeekFSM PeekBetsFSM
+            "PeekDeal" -> pure $ SomePeekFSM PeekDealFSM
+            "PeekEarlySurrender" -> pure $ SomePeekFSM PeekEarlySurrenderFSM
+            "PeekPeek" -> pure $ SomePeekFSM PeekPeekFSM
+            "PeekInsuranceDecision" -> pure $ SomePeekFSM PeekInsuranceDecisionFSM
+            "PeekInsuranceSettled" -> pure $ SomePeekFSM PeekInsuranceSettledFSM
+            "PeekPlayers" -> pure $ SomePeekFSM PeekPlayersFSM
+            "PeekDealing" -> pure $ SomePeekFSM PeekDealingFSM
+            "PeekSettle" -> pure $ SomePeekFSM PeekSettleFSM
+            "PeekComplete" -> pure $ SomePeekFSM PeekCompleteFSM
+            _ -> fail $ "Unknown PeekFSM tag: " ++ tag

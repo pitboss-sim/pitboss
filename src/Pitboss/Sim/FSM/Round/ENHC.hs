7b83942ab81ae041b6f6a74c62c39aaf079c918a
Pitboss <pitboss.sim@gmail.com>
2025-05-08 02:00:36 -0700

    sim: finite state machines

---
+++ src/Pitboss/Sim/FSM/Round/ENHC.hs
@@ -0,0 +1,82 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE StandaloneDeriving #-}
+
+module Pitboss.Sim.FSM.Round.ENHC where
+
+import Pitboss.Sim.FSM.Types.PhaseTag (PhaseTag (..), RoundPhase (..))
+import Pitboss.Sim.FSM.Types.Transitionable (TransitionPhase (..), Transitionable (..))
+
+data ENHCPhase
+  = ENHCAwaiting
+  | ENHCBets
+  | ENHCDeal
+  | ENHCEarlySurrender
+  | ENHCPlayers
+  | ENHCDealer
+  | ENHCSettle
+  | ENHCComplete
+
+data ENHCFSM (p :: ENHCPhase) where
+  ENHCAwaitingFSM :: ENHCFSM 'ENHCAwaiting
+  ENHCBetsFSM :: ENHCFSM 'ENHCBets
+  ENHCDealFSM :: ENHCFSM 'ENHCDeal
+  ENHCEarlySurrenderFSM :: ENHCFSM 'ENHCEarlySurrender
+  ENHCPlayersFSM :: ENHCFSM 'ENHCPlayers
+  ENHCDealerFSM :: ENHCFSM 'ENHCDealer
+  ENHCSettleFSM :: ENHCFSM 'ENHCSettle
+  ENHCCompleteFSM :: ENHCFSM 'ENHCComplete
+
+deriving instance Show (ENHCFSM p)
+
+deriving instance Eq (ENHCFSM p)
+
+-- advancement
+
+beginENHC :: ENHCFSM 'ENHCAwaiting -> ENHCFSM 'ENHCBets
+beginENHC ENHCAwaitingFSM = ENHCBetsFSM
+
+betsPlacedENHC :: ENHCFSM 'ENHCBets -> ENHCFSM 'ENHCDeal
+betsPlacedENHC ENHCBetsFSM = ENHCDealFSM
+
+dealCardsENHC :: ENHCFSM 'ENHCDeal -> ENHCFSM 'ENHCEarlySurrender
+dealCardsENHC ENHCDealFSM = ENHCEarlySurrenderFSM
+
+insuranceDecidedENHC :: ENHCFSM 'ENHCEarlySurrender -> ENHCFSM 'ENHCPlayers
+insuranceDecidedENHC = resolveEarlySurrenderENHC -- shim, for uniformity
+
+resolveEarlySurrenderENHC :: ENHCFSM 'ENHCEarlySurrender -> ENHCFSM 'ENHCPlayers
+resolveEarlySurrenderENHC ENHCEarlySurrenderFSM = ENHCPlayersFSM
+
+finishPlayersENHC :: ENHCFSM 'ENHCPlayers -> ENHCFSM 'ENHCDealer
+finishPlayersENHC ENHCPlayersFSM = ENHCDealerFSM
+
+finishDealerENHC :: ENHCFSM 'ENHCDealer -> ENHCFSM 'ENHCSettle
+finishDealerENHC ENHCDealerFSM = ENHCSettleFSM
+
+resolvePayoutsENHC :: ENHCFSM 'ENHCSettle -> ENHCFSM 'ENHCComplete
+resolvePayoutsENHC ENHCSettleFSM = ENHCCompleteFSM
+
+instance PhaseTag ENHCFSM RoundPhase where
+  phaseTag = \case
+    ENHCAwaitingFSM -> Awaiting
+    ENHCBetsFSM -> Bets
+    ENHCDealFSM -> Deal
+    ENHCEarlySurrenderFSM -> EarlySurrender
+    ENHCPlayersFSM -> Players
+    ENHCDealerFSM -> Dealer
+    ENHCSettleFSM -> Settle
+    ENHCCompleteFSM -> Complete
+
+instance Transitionable (ENHCFSM p) where
+  transitionType = \case
+    ENHCAwaitingFSM -> AwaitInput
+    ENHCBetsFSM -> AwaitInput
+    ENHCDealFSM -> AutoAdvance
+    ENHCEarlySurrenderFSM -> AwaitInput
+    ENHCPlayersFSM -> AwaitInput
+    ENHCDealerFSM -> AutoAdvance
+    ENHCSettleFSM -> AutoAdvance
+    ENHCCompleteFSM -> TerminalPhase
---
+++ src/Pitboss/Sim/FSM/Types/PhaseTag.hs
@@ -0,0 +1,19 @@
+{-# LANGUAGE FunctionalDependencies #-}
+
+module Pitboss.Sim.FSM.Types.PhaseTag where
+
+data RoundPhase
+  = Awaiting
+  | Bets
+  | Deal
+  | EarlySurrender
+  | InsuranceDecision
+  | InsuranceSettled
+  | Peek
+  | Players
+  | Dealer
+  | Settle
+  | Complete
+
+class PhaseTag fsm phase | fsm -> phase where
+  phaseTag :: fsm p -> phase

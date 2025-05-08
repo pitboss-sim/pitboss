b5e9957aa259c9b7b5a5213e6dcb226626f6ca0a
Pitboss <pitboss.sim@gmail.com>
2025-05-08 02:00:36 -0700

    mechanics: finite state machines

---
+++ src/Pitboss/FSM/DealerHand.hs
@@ -0,0 +1,46 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+module Pitboss.FSM.DealerHand (
+    module Pitboss.FSM.DealerHand.FSM,
+    module Pitboss.FSM.DealerHand.Phase,
+    module Pitboss.FSM.DealerHand.Transition,
+    SomeDealerHandFSM (..),
+)
+where
+
+import Data.Aeson.Types
+import Pitboss.FSM.DealerHand.FSM (DealerHandFSM (..))
+import Pitboss.FSM.DealerHand.Phase (DealerHandPhase (..), DealerHandResolution (..))
+import Pitboss.FSM.DealerHand.Transition
+
+data SomeDealerHandFSM = forall p. SomeDealerHandFSM (DealerHandFSM p)
+
+instance Show SomeDealerHandFSM where
+    show (SomeDealerHandFSM fsm) = "SomeDealerHandFSM (" ++ show fsm ++ ")"
+
+instance Eq SomeDealerHandFSM where
+    (SomeDealerHandFSM f1) == (SomeDealerHandFSM f2) = case (f1, f2) of
+        (DealingFSM, DealingFSM) -> True
+        (EvaluatingFSM, EvaluatingFSM) -> True
+        (ResolvedFSM r1, ResolvedFSM r2) -> r1 == r2
+        _ -> False
+
+instance ToJSON SomeDealerHandFSM where
+    toJSON (SomeDealerHandFSM fsm) = case fsm of
+        DealingFSM -> object ["tag" .= String "Dealing"]
+        EvaluatingFSM -> object ["tag" .= String "Evaluating"]
+        ResolvedFSM r -> object ["tag" .= String "Resolved", "resolution" .= r]
+        InterruptedFSM r -> object ["tag" .= String "Interrupted", "reason" .= r]
+
+instance FromJSON SomeDealerHandFSM where
+    parseJSON = withObject "SomeDealerHandFSM" $ \obj -> do
+        tag <- obj .: "tag"
+        case tag of
+            "Dealing" -> pure $ SomeDealerHandFSM DealingFSM
+            "Evaluating" -> pure $ SomeDealerHandFSM EvaluatingFSM
+            "Resolved" -> do
+                r <- obj .: "resolution"
+                pure $ SomeDealerHandFSM (ResolvedFSM r)
+            other -> fail $ "Unknown tag for SomeDealerHandFSM: " ++ other
---
+++ src/Pitboss/FSM/PlayerSpot.hs
@@ -0,0 +1,69 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+module Pitboss.FSM.PlayerSpot (
+    module Pitboss.FSM.PlayerSpot.FSM,
+    module Pitboss.FSM.PlayerSpot.Phase,
+    module Pitboss.FSM.PlayerSpot.Transition,
+    SomePlayerSpotFSM (..),
+    interruptSpot,
+    resumeFromInterrupt,
+)
+where
+
+import Data.Aeson.Types
+import Data.Text qualified as T
+import Pitboss.FSM.PlayerSpot.FSM
+import Pitboss.FSM.PlayerSpot.Phase
+import Pitboss.FSM.PlayerSpot.Transition
+import Pitboss.FSM.Types (InterruptReason)
+import Pitboss.FSM.Types.Transitionable
+
+data SomePlayerSpotFSM = forall p. SomePlayerSpotFSM (PlayerSpotFSM p)
+
+instance Show SomePlayerSpotFSM where
+    show (SomePlayerSpotFSM fsm) = show fsm
+
+instance Eq SomePlayerSpotFSM where
+    SomePlayerSpotFSM f1 == SomePlayerSpotFSM f2 = case (f1, f2) of
+        (SpotIdleFSM, SpotIdleFSM) -> True
+        (SpotEngagedFSM, SpotEngagedFSM) -> True
+        (SpotWaitingForHandsFSM, SpotWaitingForHandsFSM) -> True
+        (SpotResolvedFSM, SpotResolvedFSM) -> True
+        (SpotInterruptedFSM r1, SpotInterruptedFSM r2) -> r1 == r2
+        _ -> False
+
+instance ToJSON SomePlayerSpotFSM where
+    toJSON (SomePlayerSpotFSM fsm) = case fsm of
+        SpotIdleFSM -> object ["tag" .= String "SpotIdle"]
+        SpotEngagedFSM -> object ["tag" .= String "SpotEngaged"]
+        SpotWaitingForHandsFSM -> object ["tag" .= String "SpotWaitingForHands"]
+        SpotResolvedFSM -> object ["tag" .= String "SpotResolved"]
+        SpotInterruptedFSM reason ->
+            object
+                [ "tag" .= String "SpotInterrupted"
+                , "reason" .= reason
+                ]
+
+instance FromJSON SomePlayerSpotFSM where
+    parseJSON = withObject "SomePlayerSpotFSM" $ \obj -> do
+        tag <- obj .: "tag"
+        case (tag :: T.Text) of
+            "SpotIdle" -> pure $ SomePlayerSpotFSM SpotIdleFSM
+            "SpotEngaged" -> pure $ SomePlayerSpotFSM SpotEngagedFSM
+            "SpotWaitingForHands" -> pure $ SomePlayerSpotFSM SpotWaitingForHandsFSM
+            "SpotResolved" -> pure $ SomePlayerSpotFSM SpotResolvedFSM
+            "SpotInterrupted" -> do
+                reason <- obj .: "reason"
+                pure $ SomePlayerSpotFSM (SpotInterruptedFSM reason)
+            other -> fail $ "Unknown tag for SomePlayerSpotFSM: " ++ T.unpack other
+
+instance Transitionable SomePlayerSpotFSM where
+    transitionType (SomePlayerSpotFSM fsm) = transitionType fsm
+
+interruptSpot :: InterruptReason -> PlayerSpotFSM p -> SomePlayerSpotFSM
+interruptSpot reason _ = SomePlayerSpotFSM (SpotInterruptedFSM reason)
+
+resumeFromInterrupt :: PlayerSpotFSM ('SpotInterrupted r) -> PlayerSpotFSM 'SpotIdle
+resumeFromInterrupt (SpotInterruptedFSM _) = SpotIdleFSM

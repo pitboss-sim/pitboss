c708601dc60cd0d5cf3a1e0234f536aebe043a38
Pitboss <pitboss.sim@gmail.com>
2025-05-08 02:00:36 -0700

    mechanics: finite state machines

---
+++ src/Pitboss/FSM/PlayerSpot/Phase.hs
@@ -0,0 +1,36 @@
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+module Pitboss.FSM.PlayerSpot.Phase where
+
+import Data.Aeson.Types
+import Data.Text qualified as T
+import GHC.Generics
+import Pitboss.FSM.DealerRound
+
+data PlayerSpotPhase
+    = SpotIdle
+    | SpotEngaged
+    | SpotWaitingForHands
+    | SpotResolved
+    | SpotInterrupted InterruptReason
+    deriving (Eq, Show, Generic)
+
+instance ToJSON PlayerSpotPhase where
+    toJSON = \case
+        SpotIdle -> object ["tag" .= String "SpotIdle"]
+        SpotEngaged -> object ["tag" .= String "SpotEngaged"]
+        SpotWaitingForHands -> object ["tag" .= String "SpotWaitingForHands"]
+        SpotResolved -> object ["tag" .= String "SpotResolved"]
+        SpotInterrupted reason -> object ["tag" .= String "SpotInterrupted", "reason" .= reason]
+
+instance FromJSON PlayerSpotPhase where
+    parseJSON = withObject "PlayerSpotPhase" $ \obj -> do
+        tag <- obj .: "tag"
+        case tag :: T.Text of
+            "SpotIdle" -> pure SpotIdle
+            "SpotEngaged" -> pure SpotEngaged
+            "SpotWaitingForHands" -> pure SpotWaitingForHands
+            "SpotResolved" -> pure SpotResolved
+            "SpotInterrupted" -> SpotInterrupted <$> obj .: "reason"
+            _ -> fail $ "Unknown tag in PlayerSpotPhase object: " ++ T.unpack tag

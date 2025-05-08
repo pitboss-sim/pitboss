e3eda992dd218532ef343d3ce5916a5fc0131e00
Pitboss <pitboss.sim@gmail.com>
2025-05-08 04:01:11 -0700

    fixup platonic

--- src/Pitboss/Blackjack/Hand/Score.hs
+++ src/Pitboss/Blackjack/Hand/Score.hs
@@ -2,19 +2,30 @@ module Pitboss.Blackjack.Hand.Score where
 
 import Pitboss.Blackjack.Card (Card (..), Rank (..))
 import Pitboss.Blackjack.Hand (Hand (..))
+import Pitboss.Blackjack.Hand.Category (isBlackjack)
+
+data DealerScore
+  = DealerBlackjack
+  | Total Int Bool -- value, isSoft
+  deriving (Eq, Show)
+
+dealerHandTotal :: Hand -> Maybe DealerScore
+dealerHandTotal (Hand cards)
+  | isBlackjack cards = Just DealerBlackjack
+  | otherwise =
+      let base = hardHandTotal cards
+          hasAce = any ((== Ace) . rank) cards
+          soft = hasAce && base + 10 <= 21
+          total = if soft then base + 10 else base
+       in if total > 21
+            then Nothing
+            else Just (Total total soft)
 
 data Score
   = Busted
   | Blackjack
   | Valued Int
   deriving (Eq, Show)
-
-scoreHand :: Hand -> Score
-scoreHand (Hand cards) =
-  case softHandTotal cards of
-    Nothing -> Busted
-    Just 21 | length cards == 2 -> Blackjack
-    Just val -> Valued val
 
 isSoft :: Hand -> Bool
 isSoft (Hand cards) =

0205670c4d124c28c54c62d2a36cc5a3a1a2af81
Pitboss <pitboss.sim@gmail.com>
2025-05-08 02:00:36 -0700

    mechanics: finite state machines

---
+++ src/Pitboss/FSM/DealerRound/ENHC/Phase.hs
@@ -0,0 +1,12 @@
+module Pitboss.FSM.DealerRound.ENHC.Phase where
+
+data ENHCPhase
+    = ENHCAwaiting
+    | ENHCBets
+    | ENHCDeal
+    | ENHCEarlySurrender
+    | ENHCPlayers
+    | ENHCDealer
+    | ENHCSettle
+    | ENHCComplete
+    | ENHCInterrupted

5db2be3422be9c3d759c1dba2a1296e8a5c12a2d
Pitboss <pitboss.sim@gmail.com>
2025-05-08 02:00:36 -0700

    mechanics: finite state machines

---
+++ src/Pitboss/FSM/PlayerTable/FSM.hs
@@ -0,0 +1,29 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE LambdaCase #-}
+
+module Pitboss.FSM.PlayerTable.FSM where
+
+import Pitboss.FSM.PlayerTable.Phase
+import Pitboss.FSM.Types.Transitionable
+
+data PlayerTableFSM (p :: PlayerPhase) where
+    IdleFSM :: PlayerTableFSM 'Idle
+    ChoosingTableFSM :: PlayerTableFSM 'ChoosingTable
+    PlacingBetFSM :: PlayerTableFSM 'PlacingBet
+    PlayingHandFSM :: PlayerTableFSM 'PlayingHand
+    ObservingFSM :: PlayerTableFSM 'Observing
+    DoneFSM :: PlayerTableFSM 'Done
+
+deriving instance Show (PlayerTableFSM p)
+
+deriving instance Eq (PlayerTableFSM p)
+
+instance Transitionable (PlayerTableFSM p) where
+    transitionType = \case
+        IdleFSM -> AwaitInput
+        ChoosingTableFSM -> AwaitInput
+        PlacingBetFSM -> AwaitInput
+        PlayingHandFSM -> AwaitInput
+        ObservingFSM -> AutoAdvance
+        DoneFSM -> TerminalPhase

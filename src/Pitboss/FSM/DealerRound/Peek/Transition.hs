4a7f39ebdc2530a20bcfd70da653e0eb731aba45
Pitboss <pitboss.sim@gmail.com>
2025-05-08 02:00:36 -0700

    mechanics: finite state machines

---
+++ src/Pitboss/FSM/DealerRound/Peek/Transition.hs
@@ -0,0 +1,53 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE GADTs #-}
+
+module Pitboss.FSM.DealerRound.Peek.Transition where
+
+import Pitboss.Blackjack.Offering.RuleSet
+import Pitboss.FSM.DealerRound.Peek.FSM
+import Pitboss.FSM.DealerRound.Peek.Phase
+
+beginPeek :: PeekFSM 'PeekAwaiting -> PeekFSM 'PeekBets
+beginPeek PeekAwaitingFSM = PeekBetsFSM
+
+betsPlacedPeek :: PeekFSM 'PeekBets -> PeekFSM 'PeekDeal
+betsPlacedPeek PeekBetsFSM = PeekDealFSM
+
+dealCardsPeek :: PeekFSM 'PeekDeal -> PeekFSM 'PeekEarlySurrender
+dealCardsPeek PeekDealFSM = PeekEarlySurrenderFSM
+
+earlySurrenderBlackjackPeek :: PeekFSM 'PeekEarlySurrender -> PeekFSM 'PeekComplete
+earlySurrenderBlackjackPeek PeekEarlySurrenderFSM = PeekCompleteFSM
+
+resolveEarlySurrenderPeek :: PeekFSM 'PeekEarlySurrender -> PeekFSM 'PeekPeek
+resolveEarlySurrenderPeek PeekEarlySurrenderFSM = PeekPeekFSM
+
+dealerBlackjackPeek :: PeekFSM 'PeekPeek -> PeekFSM 'PeekComplete
+dealerBlackjackPeek PeekPeekFSM = PeekCompleteFSM
+
+dealerNoBlackjackPeek :: PeekFSM 'PeekPeek -> PeekFSM 'PeekInsuranceDecision
+dealerNoBlackjackPeek PeekPeekFSM = PeekInsuranceDecisionFSM
+
+maybeEnterEarlySurrenderPeek ::
+    RuleSet ->
+    PeekFSM 'PeekDeal ->
+    Either (PeekFSM 'PeekPeek) (PeekFSM 'PeekEarlySurrender)
+maybeEnterEarlySurrenderPeek rules fsm =
+    case surrender rules of
+        Early -> Right (dealCardsPeek fsm)
+        _ -> Left PeekPeekFSM
+
+insuranceDecidedPeek :: PeekFSM 'PeekInsuranceDecision -> PeekFSM 'PeekInsuranceSettled
+insuranceDecidedPeek PeekInsuranceDecisionFSM = PeekInsuranceSettledFSM
+
+proceedToPlayersPeek :: PeekFSM 'PeekInsuranceSettled -> PeekFSM 'PeekPlayers
+proceedToPlayersPeek PeekInsuranceSettledFSM = PeekPlayersFSM
+
+finishPlayersPeek :: PeekFSM 'PeekPlayers -> PeekFSM 'PeekDealer
+finishPlayersPeek PeekPlayersFSM = PeekDealerFSM
+
+finishDealerPeek :: PeekFSM 'PeekDealer -> PeekFSM 'PeekSettle
+finishDealerPeek PeekDealerFSM = PeekSettleFSM
+
+resolvePayoutsPeek :: PeekFSM 'PeekSettle -> PeekFSM 'PeekComplete
+resolvePayoutsPeek PeekSettleFSM = PeekCompleteFSM

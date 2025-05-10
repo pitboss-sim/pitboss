19ba7429d8b3e33f6c35fdb874fea33b88e14a71
Pitboss <pitboss.sim@gmail.com>
2025-05-09 18:07:23 -0700

    trace: timeline

---
+++ src/Pitboss/Trace/Entity/Dealer.hs
@@ -0,0 +1,96 @@
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+
+{-# HLINT ignore "Use newtype instead of data" #-}
+module Pitboss.Trace.Entity.Dealer where
+
+import Data.Aeson (FromJSON, ToJSON)
+import GHC.Generics (Generic)
+import Pitboss.Mechanics.Dealer.Types
+import Pitboss.Mechanics.Dealer.Types.DealerHandFSM
+import Pitboss.Mechanics.Dealer.Types.DealerRoundFSM
+import Pitboss.Mechanics.Dealer.Types.DealerTableFSM
+import Pitboss.Trace.Entity.Capabilities.Clocked
+import Pitboss.Trace.Entity.Table hiding (_tick)
+import Pitboss.Trace.Timeline.EntityRef
+import Pitboss.Trace.Timeline.Identifier
+
+data DealerEntity = DealerEntity
+  { _tick :: Tick,
+    _state :: DealerState,
+    _fsm :: SomeDealerFSM,
+    _rels :: DealerRelations
+  }
+  deriving (Generic)
+
+instance Show DealerEntity where
+  show (DealerEntity t s f r) =
+    "DealerEntity { _tick = "
+      ++ show t
+      ++ ", _state = "
+      ++ show s
+      ++ ", _fsm = "
+      ++ show f
+      ++ ", _rels = "
+      ++ show r
+      ++ " }"
+
+instance Clocked DealerEntity where
+  tick = _tick
+  setTick t d = d {_tick = t}
+
+instance ToJSON DealerEntity
+
+instance FromJSON DealerEntity
+
+data DealerState = DealerState
+  { _dealerName :: String,
+    _assignedTable :: Maybe (EntityRef TableState)
+  }
+  deriving (Eq, Show, Generic)
+
+instance ToJSON DealerState
+
+instance FromJSON DealerState
+
+data DealerDelta
+  = DealerStateDelta DealerStateChange
+  | DealerRelationDelta DealerRelationChange
+  deriving (Eq, Show, Generic)
+
+instance ToJSON DealerDelta
+
+instance FromJSON DealerDelta
+
+data DealerStateChange
+  = RenameDealer String
+  | SetDealerTableFSM SomeDealerTableFSM
+  | SetDealerRoundFSM SomeDealerRoundFSM
+  | SetDealerHandFSM SomeDealerHandFSM
+  deriving (Eq, Show, Generic)
+
+instance ToJSON DealerStateChange
+
+instance FromJSON DealerStateChange
+
+data DealerRelationChange
+  = AssignTable TableId
+  | UnassignTable
+  | AssignRound RoundId
+  | UnassignRound
+  | AssignHand DealerHandId
+  | UnassignHand
+  deriving (Eq, Show, Generic)
+
+instance ToJSON DealerRelationChange
+
+instance FromJSON DealerRelationChange
+
+data DealerRelations = DealerRelations
+  { _currentRound :: Maybe RoundId,
+    _activeHand :: Maybe DealerHandId
+  }
+  deriving (Eq, Show, Generic)
+
+instance ToJSON DealerRelations
+
+instance FromJSON DealerRelations

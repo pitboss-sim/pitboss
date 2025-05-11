739bd2872df8c9c8286891ecae6b334b23bda8f8
Pitboss <pitboss.sim@gmail.com>
2025-05-11 03:29:51 -0700

    trace: post-fsm entities

--- src/Pitboss/Trace/Entity/Capabilities/Reversible.hs
+++ src/Pitboss/Trace/Entity/Capabilities/Reversible.hs
@@ -1,4 +1,8 @@
+{-# LANGUAGE LambdaCase #-}
+
 module Pitboss.Trace.Entity.Capabilities.Reversible where
+
+import Pitboss.Trace.Entity.PlayerHand
 
 class Reversible d where
   invert :: d -> Maybe d
@@ -8,3 +12,11 @@ data InversionError
   | MissingPriorContext String
   | CustomReason String
   deriving (Eq, Show)
+
+instance Reversible PlayerHandDelta where
+  invert = \case
+    AddCard c -> Just (RemoveCard c)
+    RemoveCard c -> Just (AddCard c)
+    ReplaceCards old new -> Just (ReplaceCards new old)
+    ReplacePlayerHandIndex from to -> Just (ReplacePlayerHandIndex to from)
+    ReplaceSplitDepth from to -> Just (ReplaceSplitDepth to from)

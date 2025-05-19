adab4900a69af8af224bc1cb1054e151f70b62ba
Pitboss <pitboss.sim@gmail.com>
2025-05-18 20:25:43 -0700

    trace: deltas

---
+++ src/Pitboss/Trace/Entity/PlayerSpot/Delta.hs
@@ -0,0 +1,37 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeFamilies #-}
+
+module Pitboss.Trace.Entity.PlayerSpot.Delta where
+
+import Data.Aeson (FromJSON, ToJSON)
+import GHC.Generics (Generic)
+import Pitboss.Blackjack.Chips
+import Pitboss.FSM.PlayerSpot
+import Pitboss.Trace.Entity.PlayerSpot.Entity
+import Pitboss.Trace.Entity.Types.EntityId
+import Pitboss.Trace.Entity.Types.FiniteMap.Occupancy
+
+data PlayerSpotEntityAttrsDelta
+    = ReplaceWager Chips Chips
+    deriving (Eq, Show, Generic)
+
+instance ToJSON PlayerSpotEntityAttrsDelta
+instance FromJSON PlayerSpotEntityAttrsDelta
+
+data PlayerSpotEntityModesDelta
+    = ReplaceFSM SomePlayerSpotFSM SomePlayerSpotFSM
+    deriving (Eq, Show, Generic)
+
+instance ToJSON PlayerSpotEntityModesDelta
+instance FromJSON PlayerSpotEntityModesDelta
+
+data PlayerSpotEntityRelsDelta
+    = UpdatePlayer (ClockedRef PlayerEntityId) (ClockedRef PlayerEntityId)
+    | UpdateRound (ClockedRef DealerRoundEntityId) (ClockedRef DealerRoundEntityId)
+    | UpdateHandOccupancy
+        (PlayerSpotHandIx, Occupancy (ClockedRef PlayerHandEntityId))
+        (PlayerSpotHandIx, Occupancy (ClockedRef PlayerHandEntityId))
+    deriving (Eq, Show, Generic)
+
+instance ToJSON PlayerSpotEntityRelsDelta
+instance FromJSON PlayerSpotEntityRelsDelta
---
+++ src/Pitboss/Trace/Entity/TableShoe/Delta.hs
@@ -0,0 +1,1 @@
+module Pitboss.Trace.Entity.TableShoe.Delta where

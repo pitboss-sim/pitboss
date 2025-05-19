e095f3a86c4427c16b51ad7ea79be166de565998
Pitboss <pitboss.sim@gmail.com>
2025-05-18 20:25:43 -0700

    trace: deltas

---
+++ src/Pitboss/Trace/Entity/PlayerHand/Delta.hs
@@ -0,0 +1,34 @@
+module Pitboss.Trace.Entity.PlayerHand.Delta where
+
+import Data.Aeson (FromJSON, ToJSON)
+import GHC.Generics (Generic)
+import Pitboss.Blackjack.Card
+import Pitboss.FSM.PlayerHand
+import Pitboss.Trace.Entity.Types.EntityId
+
+data PlayerHandEntityAttrsDelta
+    = AddCard Card
+    | RemoveCard Card
+    | ReplaceCards [Card] [Card]
+    | ReplacePlayerHandIndex Int Int
+    | ReplaceSplitDepth Int Int
+    deriving (Eq, Show, Generic)
+
+instance ToJSON PlayerHandEntityAttrsDelta
+instance FromJSON PlayerHandEntityAttrsDelta
+
+data PlayerHandEntityModesDelta
+    = ReplaceFSM SomePlayerHandFSM SomePlayerHandFSM
+    deriving (Eq, Show, Generic)
+
+instance ToJSON PlayerHandEntityModesDelta
+instance FromJSON PlayerHandEntityModesDelta
+
+data PlayerHandEntityRelsDelta
+    = UpdatePlayerSpot (ClockedRef PlayerSpotEntityId) (ClockedRef PlayerSpotEntityId)
+    | UpdateDealerRound (ClockedRef DealerRoundEntityId) (ClockedRef DealerRoundEntityId)
+    | UpdatePlayer (ClockedRef PlayerEntityId) (ClockedRef PlayerEntityId)
+    deriving (Eq, Show, Generic)
+
+instance ToJSON PlayerHandEntityRelsDelta
+instance FromJSON PlayerHandEntityRelsDelta

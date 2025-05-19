7137345e5dd70d7801bd4eea3db9e996a2858888
Pitboss <pitboss.sim@gmail.com>
2025-05-18 17:42:58 -0700

    trace: collocated top-level delta definitions

--- src/Pitboss/Trace/Entity/Offering/Delta.hs
+++ src/Pitboss/Trace/Entity/Offering/Delta.hs
@@ -1,52 +1,10 @@
-{-# LANGUAGE GADTs #-}
-{-# LANGUAGE LambdaCase #-}
-{-# LANGUAGE TypeFamilies #-}
-{-# OPTIONS_GHC -Wno-dodgy-exports #-}
-{-# OPTIONS_GHC -Wno-unused-imports #-}
-
 module Pitboss.Trace.Entity.Offering.Delta (
     module Pitboss.Trace.Entity.Offering.Delta.Attrs,
     module Pitboss.Trace.Entity.Offering.Delta.Modes,
     module Pitboss.Trace.Entity.Offering.Delta.Rels,
-    OfferingEntityDelta (..),
 )
 where
 
-import Data.Aeson (FromJSON, ToJSON)
-import GHC.Generics (Generic)
-import Pitboss.Trace.Entity
-import Pitboss.Trace.Entity.Capabilities
 import Pitboss.Trace.Entity.Offering.Delta.Attrs
 import Pitboss.Trace.Entity.Offering.Delta.Modes
 import Pitboss.Trace.Entity.Offering.Delta.Rels
-
-data OfferingEntityDelta
-    = OfferingEntityAttrsDelta OfferingEntityAttrsDelta
-    | OfferingEntityModesDelta OfferingEntityModesDelta
-    | OfferingEntityRelsDelta OfferingEntityRelsDelta
-    deriving (Eq, Show, Generic)
-
-instance ToJSON OfferingEntityDelta
-
-instance FromJSON OfferingEntityDelta
-
-instance Incremental OfferingEntityDelta where
-    type Target OfferingEntityDelta = OfferingEntity
-
-    applyDelta delta entity = case delta of
-        OfferingEntityAttrsDelta d -> entity{_offeringEntityAttrs = applyDelta d (_offeringEntityAttrs entity)}
-        OfferingEntityModesDelta d -> entity{_offeringEntityModes = applyDelta d (_offeringEntityModes entity)}
-        OfferingEntityRelsDelta d -> entity{_offeringEntityRels = applyDelta d (_offeringEntityRels entity)}
-
-    previewDelta delta entity = Just $ applyDelta delta entity
-
-    describeDelta delta entity = case delta of
-        OfferingEntityAttrsDelta d -> describeDelta d (_offeringEntityAttrs entity)
-        OfferingEntityModesDelta d -> describeDelta d (_offeringEntityModes entity)
-        OfferingEntityRelsDelta d -> describeDelta d (_offeringEntityRels entity)
-
-instance Reversible OfferingEntityDelta where
-    invert = \case
-        OfferingEntityAttrsDelta d -> OfferingEntityAttrsDelta <$> invert d
-        OfferingEntityModesDelta d -> OfferingEntityModesDelta <$> invert d
-        OfferingEntityRelsDelta d -> OfferingEntityRelsDelta <$> invert d

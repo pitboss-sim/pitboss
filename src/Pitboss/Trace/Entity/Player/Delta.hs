29f8c69d310b596d0309b3a12cbcbfc995007e98
Pitboss <pitboss.sim@gmail.com>
2025-05-18 20:25:43 -0700

    trace: deltas

---
+++ src/Pitboss/Trace/Entity/Player/Delta.hs
@@ -0,0 +1,9 @@
+module Pitboss.Trace.Entity.Player.Delta (
+    module Pitboss.Trace.Entity.Player.Delta.Attrs,
+    module Pitboss.Trace.Entity.Player.Delta.Modes,
+    module Pitboss.Trace.Entity.Player.Delta.Rels,
+) where
+
+import Pitboss.Trace.Entity.Player.Delta.Attrs
+import Pitboss.Trace.Entity.Player.Delta.Modes
+import Pitboss.Trace.Entity.Player.Delta.Rels

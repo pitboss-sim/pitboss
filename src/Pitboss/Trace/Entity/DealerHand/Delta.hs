1530362bdd848d1a6465d0c7bfd51caa1861449b
Pitboss <pitboss.sim@gmail.com>
2025-05-19 21:05:19 -0700

    delta stuff wip

--- src/Pitboss/Trace/Entity/DealerHand/Delta.hs
+++ src/Pitboss/Trace/Entity/DealerHand/Delta.hs
@@ -1,10 +1,35 @@
-module Pitboss.Trace.Entity.DealerHand.Delta (
-    module Pitboss.Trace.Entity.DealerHand.Delta.Attrs,
-    module Pitboss.Trace.Entity.DealerHand.Delta.Modes,
-    module Pitboss.Trace.Entity.DealerHand.Delta.Rels,
-)
-where
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
 
-import Pitboss.Trace.Entity.DealerHand.Delta.Attrs
-import Pitboss.Trace.Entity.DealerHand.Delta.Modes
-import Pitboss.Trace.Entity.DealerHand.Delta.Rels
+{-# HLINT ignore "Use newtype instead of data" #-}
+module Pitboss.Trace.Entity.DealerHand.Delta where
+
+import Data.Aeson
+import GHC.Generics
+import Pitboss.Blackjack.Card
+import Pitboss.FSM.DealerHand
+import Pitboss.Trace.Entity.Types.EntityId
+
+data DealerHandEntityAttrsDelta
+    = AddCard Card
+    | RemoveCard Card
+    | ReplaceCards [Card] [Card] -- old, new
+    deriving (Eq, Show, Generic)
+
+instance ToJSON DealerHandEntityAttrsDelta
+instance FromJSON DealerHandEntityAttrsDelta
+
+data DealerHandEntityModesDelta
+    = ReplaceFSM SomeDealerHandFSM SomeDealerHandFSM
+    deriving (Eq, Show, Generic)
+
+instance ToJSON DealerHandEntityModesDelta
+instance FromJSON DealerHandEntityModesDelta
+
+data DealerHandEntityRelsDelta
+    = UpdatePlayerSpot (ClockedRef PlayerSpotEntityId) (ClockedRef PlayerSpotEntityId)
+    | UpdateDealerRound (ClockedRef DealerRoundEntityId) (ClockedRef DealerRoundEntityId)
+    | UpdateDealer (ClockedRef DealerEntityId) (ClockedRef DealerEntityId)
+    deriving (Eq, Show, Generic)
+
+instance ToJSON DealerHandEntityRelsDelta
+instance FromJSON DealerHandEntityRelsDelta

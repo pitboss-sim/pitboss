b75aba8433b688b8ad605d344f8762c4986eaca6
Pitboss <pitboss.sim@gmail.com>
2025-05-24 16:47:30 -0700

    causality: trace, timeline, and tick cache

---
+++ src/Pitboss/Causality/Registry.hs
@@ -0,0 +1,34 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE UndecidableInstances #-}
+
+module Pitboss.Causality.Registry (
+    Registry (..),
+) where
+
+import Data.Aeson
+import Data.HashMap.Strict.InsOrd (InsOrdHashMap)
+import Pitboss.Causality.Timeline
+import Pitboss.Causality.Types.Core
+
+newtype Registry (k :: EntityKind) delta = Registry
+    { unRegistry :: InsOrdHashMap (EntityId k) (Timeline k delta)
+    }
+
+deriving instance (Eq a, Eq (Timeline k a)) => Eq (Registry k a)
+deriving instance (Show a, Show (Timeline k a)) => Show (Registry k a)
+
+instance (ToJSON (Timeline k delta)) => ToJSON (Registry k delta) where
+    toJSON (Registry m) = toJSON m
+
+instance (FromJSON (Timeline k delta)) => FromJSON (Registry k delta) where
+    parseJSON v = Registry <$> parseJSON v
+
+instance Monoid (Registry k delta) where
+    mempty = Registry mempty
+
+instance Semigroup (Registry k delta) where
+    Registry a <> Registry b = Registry (a <> b)

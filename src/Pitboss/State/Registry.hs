db7db40e58078fe47fa57395edf16d702f54e2e4
Pitboss <pitboss.sim@gmail.com>
2025-05-24 16:47:30 -0700

    state: trace, timeline, and tick cache

---
+++ src/Pitboss/State/Registry.hs
@@ -0,0 +1,39 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE UndecidableInstances #-}
+
+module Pitboss.State.Registry (
+    Registry (..),
+) where
+
+-- timeline utilities
+
+import Data.Aeson
+import Data.HashMap.Strict.InsOrd (InsOrdHashMap)
+import Data.Word (Word64)
+import Pitboss.State.Entity.Types
+import Pitboss.State.Timeline
+
+-- delta timeline per type
+
+newtype Registry (k :: EntityKind) delta = Registry
+    { unRegistry :: InsOrdHashMap Word64 (Timeline k delta)
+    }
+
+deriving instance (Eq delta) => Eq (Registry k delta)
+deriving instance (Show delta) => Show (Registry k delta)
+
+instance (ToJSON (Timeline k delta)) => ToJSON (Registry k delta) where
+    toJSON (Registry m) = toJSON m
+
+instance (FromJSON (Timeline k delta)) => FromJSON (Registry k delta) where
+    parseJSON v = Registry <$> parseJSON v
+
+instance Monoid (Registry k delta) where
+    mempty = Registry mempty
+
+instance Semigroup (Registry k delta) where
+    Registry a <> Registry b = Registry (a <> b)

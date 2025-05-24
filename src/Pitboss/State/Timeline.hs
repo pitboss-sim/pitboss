0539b2587fc0b5b6527c4777fe7f1f4f57bf3550
Pitboss <pitboss.sim@gmail.com>
2025-05-24 16:47:30 -0700

    trace: trace

---
+++ src/Pitboss/State/Timeline.hs
@@ -0,0 +1,28 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE UndecidableInstances #-}
+
+module Pitboss.State.Timeline (
+    Timeline (..),
+) where
+
+import Data.Aeson
+import Data.HashMap.Strict.InsOrd (InsOrdHashMap)
+import GHC.Generics (Generic)
+import Pitboss.State.Types.Tick
+
+newtype Timeline a where
+    Timeline :: {unTimeline :: InsOrdHashMap Tick [a]} -> Timeline a
+    deriving (Generic, Show, Eq)
+
+instance Semigroup (Timeline a) where
+    Timeline a <> Timeline b = Timeline (a <> b)
+
+instance Monoid (Timeline a) where
+    mempty = Timeline mempty
+
+instance (ToJSON a) => ToJSON (Timeline a) where
+    toJSON (Timeline m) = toJSON m
+
+instance (FromJSON a) => FromJSON (Timeline a) where
+    parseJSON v = Timeline <$> parseJSON v

91becba7141be57c5f8d5f480becbf576147087c
Pitboss <pitboss.sim@gmail.com>
2025-05-25 04:31:14 -0700

    fixup entity

--- src/Pitboss/State/Entity/Instances/Replaceable.hs
+++ src/Pitboss/State/Entity/Instances/Replaceable.hs
@@ -23,72 +23,72 @@ class ReplaceableRels (k :: EntityKind) where
 
 -- Dealer
 instance ReplaceableAttrs 'Dealer where
-    replaceAttrs newAttrs (EDealer m _ mo r) = EDealer m newAttrs mo r
+    replaceAttrs newAttrs (EDealer _ mo r) = EDealer newAttrs mo r
 instance ReplaceableModes 'Dealer where
-    replaceModes newModes (EDealer m a _ r) = EDealer m a newModes r
+    replaceModes newModes (EDealer a _ r) = EDealer a newModes r
 instance ReplaceableRels 'Dealer where
-    replaceRels newRels (EDealer m a mo _) = EDealer m a mo newRels
+    replaceRels newRels (EDealer a mo _) = EDealer a mo newRels
 
 -- DealerHand
 instance ReplaceableAttrs 'DealerHand where
-    replaceAttrs newAttrs (EDealerHand m _ mo r) = EDealerHand m newAttrs mo r
+    replaceAttrs newAttrs (EDealerHand _ mo r) = EDealerHand newAttrs mo r
 instance ReplaceableModes 'DealerHand where
-    replaceModes newModes (EDealerHand m a _ r) = EDealerHand m a newModes r
+    replaceModes newModes (EDealerHand a _ r) = EDealerHand a newModes r
 instance ReplaceableRels 'DealerHand where
-    replaceRels newRels (EDealerHand m a mo _) = EDealerHand m a mo newRels
+    replaceRels newRels (EDealerHand a mo _) = EDealerHand a mo newRels
 
 -- DealerRound
 instance ReplaceableAttrs 'DealerRound where
-    replaceAttrs newAttrs (EDealerRound m _ mo r) = EDealerRound m newAttrs mo r
+    replaceAttrs newAttrs (EDealerRound _ mo r) = EDealerRound newAttrs mo r
 instance ReplaceableModes 'DealerRound where
-    replaceModes newModes (EDealerRound m a _ r) = EDealerRound m a newModes r
+    replaceModes newModes (EDealerRound a _ r) = EDealerRound a newModes r
 instance ReplaceableRels 'DealerRound where
-    replaceRels newRels (EDealerRound m a mo _) = EDealerRound m a mo newRels
+    replaceRels newRels (EDealerRound a mo _) = EDealerRound a mo newRels
 
 -- Offering
 instance ReplaceableAttrs 'Offering where
-    replaceAttrs newAttrs (EOffering m _ mo r) = EOffering m newAttrs mo r
+    replaceAttrs newAttrs (EOffering _ mo r) = EOffering newAttrs mo r
 instance ReplaceableModes 'Offering where
-    replaceModes newModes (EOffering m a _ r) = EOffering m a newModes r
+    replaceModes newModes (EOffering a _ r) = EOffering a newModes r
 instance ReplaceableRels 'Offering where
-    replaceRels newRels (EOffering m a mo _) = EOffering m a mo newRels
+    replaceRels newRels (EOffering a mo _) = EOffering a mo newRels
 
 -- Player
 instance ReplaceableAttrs 'Player where
-    replaceAttrs newAttrs (EPlayer m _ mo r) = EPlayer m newAttrs mo r
+    replaceAttrs newAttrs (EPlayer _ mo r) = EPlayer newAttrs mo r
 instance ReplaceableModes 'Player where
-    replaceModes newModes (EPlayer m a _ r) = EPlayer m a newModes r
+    replaceModes newModes (EPlayer a _ r) = EPlayer a newModes r
 instance ReplaceableRels 'Player where
-    replaceRels newRels (EPlayer m a mo _) = EPlayer m a mo newRels
+    replaceRels newRels (EPlayer a mo _) = EPlayer a mo newRels
 
 -- PlayerHand
 instance ReplaceableAttrs 'PlayerHand where
-    replaceAttrs newAttrs (EPlayerHand m _ mo r) = EPlayerHand m newAttrs mo r
+    replaceAttrs newAttrs (EPlayerHand _ mo r) = EPlayerHand newAttrs mo r
 instance ReplaceableModes 'PlayerHand where
-    replaceModes newModes (EPlayerHand m a _ r) = EPlayerHand m a newModes r
+    replaceModes newModes (EPlayerHand a _ r) = EPlayerHand a newModes r
 instance ReplaceableRels 'PlayerHand where
-    replaceRels newRels (EPlayerHand m a mo _) = EPlayerHand m a mo newRels
+    replaceRels newRels (EPlayerHand a mo _) = EPlayerHand a mo newRels
 
 -- PlayerSpot
 instance ReplaceableAttrs 'PlayerSpot where
-    replaceAttrs newAttrs (EPlayerSpot m _ mo r) = EPlayerSpot m newAttrs mo r
+    replaceAttrs newAttrs (EPlayerSpot _ mo r) = EPlayerSpot newAttrs mo r
 instance ReplaceableModes 'PlayerSpot where
-    replaceModes newModes (EPlayerSpot m a _ r) = EPlayerSpot m a newModes r
+    replaceModes newModes (EPlayerSpot a _ r) = EPlayerSpot a newModes r
 instance ReplaceableRels 'PlayerSpot where
-    replaceRels newRels (EPlayerSpot m a mo _) = EPlayerSpot m a mo newRels
+    replaceRels newRels (EPlayerSpot a mo _) = EPlayerSpot a mo newRels
 
 -- Table
 instance ReplaceableAttrs 'Table where
-    replaceAttrs newAttrs (ETable m _ mo r) = ETable m newAttrs mo r
+    replaceAttrs newAttrs (ETable _ mo r) = ETable newAttrs mo r
 instance ReplaceableModes 'Table where
-    replaceModes newModes (ETable m a _ r) = ETable m a newModes r
+    replaceModes newModes (ETable a _ r) = ETable a newModes r
 instance ReplaceableRels 'Table where
-    replaceRels newRels (ETable m a mo _) = ETable m a mo newRels
+    replaceRels newRels (ETable a mo _) = ETable a mo newRels
 
 -- TableShoe
 instance ReplaceableAttrs 'TableShoe where
-    replaceAttrs newAttrs (ETableShoe m _ mo r) = ETableShoe m newAttrs mo r
+    replaceAttrs newAttrs (ETableShoe _ mo r) = ETableShoe newAttrs mo r
 instance ReplaceableModes 'TableShoe where
-    replaceModes newModes (ETableShoe m a _ r) = ETableShoe m a newModes r
+    replaceModes newModes (ETableShoe a _ r) = ETableShoe a newModes r
 instance ReplaceableRels 'TableShoe where
-    replaceRels newRels (ETableShoe m a mo _) = ETableShoe m a mo newRels
+    replaceRels newRels (ETableShoe a mo _) = ETableShoe a mo newRels

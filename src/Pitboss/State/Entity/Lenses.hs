f8f2ed92d9f1a8b49c40f135a5f58b5e490a5227
Pitboss <pitboss.sim@gmail.com>
2025-05-26 00:35:37 -0700

    # This is a combination of 4 commits. # This is the 1st commit message:
    wip
    
    # The commit message #2 will be skipped:
    
    # type simplification
    
    # The commit message #3 will be skipped:
    
    # wip entity cache
    
    # The commit message #4 will be skipped:
    
    # previously "claude code support" has more

---
+++ src/Pitboss/State/Entity/Lenses.hs
@@ -0,0 +1,266 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE RankNTypes #-}
+
+module Pitboss.State.Entity.Lenses where
+
+import Control.Lens hiding (ix)
+import Data.Map.Strict
+import Pitboss.Blackjack.Card (Card)
+import Pitboss.Blackjack.Chips (Chips)
+import Pitboss.Blackjack.Offering qualified as O
+import Pitboss.FSM.DealerHand
+import Pitboss.FSM.DealerRound
+import Pitboss.FSM.DealerTable
+import Pitboss.FSM.PlayerHand (SomePlayerHandFSM)
+import Pitboss.FSM.PlayerSpot (SomePlayerSpotFSM)
+import Pitboss.FSM.PlayerTable (SomePlayerTableFSM)
+import Pitboss.FSM.Table (SomeTableFSM)
+import Pitboss.State.Entity.Types
+import Pitboss.State.Types.FiniteMap (FiniteMap)
+import Pitboss.State.Types.FiniteMap.Occupancy (Occupancy)
+import Prelude hiding (round)
+
+-- EntityState lenses (for accessing Attrs/Modes/Rels from complete entities)
+
+-- Dealer
+dAttrs :: Lens' (EntityState 'Dealer) DealerAttrs
+dAttrs f (EDealer a m r) = fmap (\a' -> EDealer a' m r) (f a)
+
+dModes :: Lens' (EntityState 'Dealer) DealerModes
+dModes f (EDealer a m r) = fmap (\m' -> EDealer a m' r) (f m)
+
+dRels :: Lens' (EntityState 'Dealer) DealerRels
+dRels f (EDealer a m r) = fmap (EDealer a m) (f r)
+
+-- DealerHand
+dhAttrs :: Lens' (EntityState 'DealerHand) DealerHandAttrs
+dhAttrs f (EDealerHand a m r) = fmap (\a' -> EDealerHand a' m r) (f a)
+
+dhModes :: Lens' (EntityState 'DealerHand) DealerHandModes
+dhModes f (EDealerHand a m r) = fmap (\m' -> EDealerHand a m' r) (f m)
+
+dhRels :: Lens' (EntityState 'DealerHand) DealerHandRels
+dhRels f (EDealerHand a m r) = fmap (EDealerHand a m) (f r)
+
+-- DealerRound
+drAttrs :: Lens' (EntityState 'DealerRound) DealerRoundAttrs
+drAttrs f (EDealerRound a m r) = fmap (\a' -> EDealerRound a' m r) (f a)
+
+drModes :: Lens' (EntityState 'DealerRound) DealerRoundModes
+drModes f (EDealerRound a m r) = fmap (\m' -> EDealerRound a m' r) (f m)
+
+drRels :: Lens' (EntityState 'DealerRound) DealerRoundRels
+drRels f (EDealerRound a m r) = fmap (EDealerRound a m) (f r)
+
+-- Offering
+oAttrs :: Lens' (EntityState 'Offering) OfferingAttrs
+oAttrs f (EOffering a m r) = fmap (\a' -> EOffering a' m r) (f a)
+
+oModes :: Lens' (EntityState 'Offering) OfferingModes
+oModes f (EOffering a m r) = fmap (\m' -> EOffering a m' r) (f m)
+
+oRels :: Lens' (EntityState 'Offering) OfferingRels
+oRels f (EOffering a m r) = fmap (EOffering a m) (f r)
+
+-- Player
+pAttrs :: Lens' (EntityState 'Player) PlayerAttrs
+pAttrs f (EPlayer a m r) = fmap (\a' -> EPlayer a' m r) (f a)
+
+pModes :: Lens' (EntityState 'Player) PlayerModes
+pModes f (EPlayer a m r) = fmap (\m' -> EPlayer a m' r) (f m)
+
+pRels :: Lens' (EntityState 'Player) PlayerRels
+pRels f (EPlayer a m r) = fmap (EPlayer a m) (f r)
+
+-- PlayerHand
+phAttrs :: Lens' (EntityState 'PlayerHand) PlayerHandAttrs
+phAttrs f (EPlayerHand a m r) = fmap (\a' -> EPlayerHand a' m r) (f a)
+
+phModes :: Lens' (EntityState 'PlayerHand) PlayerHandModes
+phModes f (EPlayerHand a m r) = fmap (\m' -> EPlayerHand a m' r) (f m)
+
+phRels :: Lens' (EntityState 'PlayerHand) PlayerHandRels
+phRels f (EPlayerHand a m r) = fmap (EPlayerHand a m) (f r)
+
+-- PlayerSpot
+psAttrs :: Lens' (EntityState 'PlayerSpot) PlayerSpotAttrs
+psAttrs f (EPlayerSpot a m r) = fmap (\a' -> EPlayerSpot a' m r) (f a)
+
+psModes :: Lens' (EntityState 'PlayerSpot) PlayerSpotModes
+psModes f (EPlayerSpot a m r) = fmap (\m' -> EPlayerSpot a m' r) (f m)
+
+psRels :: Lens' (EntityState 'PlayerSpot) PlayerSpotRels
+psRels f (EPlayerSpot a m r) = fmap (EPlayerSpot a m) (f r)
+
+-- Table
+tAttrs :: Lens' (EntityState 'Table) TableAttrs
+tAttrs f (ETable a m r) = fmap (\a' -> ETable a' m r) (f a)
+
+tModes :: Lens' (EntityState 'Table) TableModes
+tModes f (ETable a m r) = fmap (\m' -> ETable a m' r) (f m)
+
+tRels :: Lens' (EntityState 'Table) TableRels
+tRels f (ETable a m r) = fmap (ETable a m) (f r)
+
+-- TableShoe
+tsAttrs :: Lens' (EntityState 'TableShoe) TableShoeAttrs
+tsAttrs f (ETableShoe a m r) = fmap (\a' -> ETableShoe a' m r) (f a)
+
+tsModes :: Lens' (EntityState 'TableShoe) TableShoeModes
+tsModes f (ETableShoe a m r) = fmap (\m' -> ETableShoe a m' r) (f m)
+
+tsRels :: Lens' (EntityState 'TableShoe) TableShoeRels
+tsRels f (ETableShoe a m r) = fmap (ETableShoe a m) (f r)
+
+-- Component type lenses (for accessing fields within Attrs/Modes/Rels)
+
+-- DealerAttrs
+dAttrsName :: Lens' DealerAttrs String
+dAttrsName f (DealerAttrs name) = fmap DealerAttrs (f name)
+
+-- DealerModes
+dModesDealerTable :: Lens' DealerModes SomeDealerTableFSM
+dModesDealerTable f (DealerModes table round hand) = fmap (\t -> DealerModes t round hand) (f table)
+
+dModesDealerRound :: Lens' DealerModes DealerRoundFSM
+dModesDealerRound f (DealerModes table round hand) = fmap (\r -> DealerModes table r hand) (f round)
+
+dModesDealerHand :: Lens' DealerModes SomeDealerHandFSM
+dModesDealerHand f (DealerModes table round hand) = fmap (DealerModes table round) (f hand)
+
+-- DealerRels
+dRelsActiveTable :: Lens' DealerRels (Maybe (EntityId 'Table))
+dRelsActiveTable f (DealerRels table round hand) = fmap (\t -> DealerRels t round hand) (f table)
+
+dRelsActiveRound :: Lens' DealerRels (Maybe (EntityId 'DealerRound))
+dRelsActiveRound f (DealerRels table round hand) = fmap (\r -> DealerRels table r hand) (f round)
+
+dRelsActiveHand :: Lens' DealerRels (Maybe (EntityId 'DealerHand))
+dRelsActiveHand f (DealerRels table round hand) = fmap (DealerRels table round) (f hand)
+
+-- DealerHandAttrs
+dhAttrsHandCards :: Lens' DealerHandAttrs [Card]
+dhAttrsHandCards f (DealerHandAttrs cards) = fmap DealerHandAttrs (f cards)
+
+-- DealerHandModes
+dhModesDealerHand :: Lens' DealerHandModes SomeDealerHandFSM
+dhModesDealerHand f (DealerHandModes fsm) = fmap DealerHandModes (f fsm)
+
+-- DealerHandRels
+dhRelsDealerRound :: Lens' DealerHandRels (EntityId 'DealerRound)
+dhRelsDealerRound f (DealerHandRels round dealer) = fmap (`DealerHandRels` dealer) (f round)
+
+dhRelsDealer :: Lens' DealerHandRels (EntityId 'Dealer)
+dhRelsDealer f (DealerHandRels round dealer) = fmap (DealerHandRels round) (f dealer)
+
+-- DealerRoundAttrs
+drAttrsNumber :: Lens' DealerRoundAttrs Int
+drAttrsNumber f (DealerRoundAttrs number active) = fmap (`DealerRoundAttrs` active) (f number)
+
+drAttrsIsActive :: Lens' DealerRoundAttrs Bool
+drAttrsIsActive f (DealerRoundAttrs number active) = fmap (DealerRoundAttrs number) (f active)
+
+-- DealerRoundRels
+drRelsTableShoeUsed :: Lens' DealerRoundRels (EntityId 'TableShoe)
+drRelsTableShoeUsed f (DealerRoundRels shoe) = fmap DealerRoundRels (f shoe)
+
+-- OfferingAttrs
+oAttrsOffering :: Lens' OfferingAttrs O.Offering
+oAttrsOffering f (OfferingAttrs offering) = fmap OfferingAttrs (f offering)
+
+-- PlayerAttrs
+pAttrsName :: Lens' PlayerAttrs String
+pAttrsName f (PlayerAttrs name bankroll) = fmap (`PlayerAttrs` bankroll) (f name)
+
+pAttrsBankroll :: Lens' PlayerAttrs Chips
+pAttrsBankroll f (PlayerAttrs name bankroll) = fmap (PlayerAttrs name) (f bankroll)
+
+-- PlayerModes
+pModesPlayerTable :: Lens' PlayerModes SomePlayerTableFSM
+pModesPlayerTable f (PlayerModes table spot hand) = fmap (\t -> PlayerModes t spot hand) (f table)
+
+pModesPlayerSpot :: Lens' PlayerModes SomePlayerSpotFSM
+pModesPlayerSpot f (PlayerModes table spot hand) = fmap (\s -> PlayerModes table s hand) (f spot)
+
+pModesPlayerHand :: Lens' PlayerModes SomePlayerHandFSM
+pModesPlayerHand f (PlayerModes table spot hand) = fmap (PlayerModes table spot) (f hand)
+
+-- PlayerHandAttrs
+phAttrsHandCards :: Lens' PlayerHandAttrs [Card]
+phAttrsHandCards f (PlayerHandAttrs cards bet depth ix) = fmap (\c -> PlayerHandAttrs c bet depth ix) (f cards)
+
+phAttrsOriginalBet :: Lens' PlayerHandAttrs Chips
+phAttrsOriginalBet f (PlayerHandAttrs cards bet depth ix) = fmap (\b -> PlayerHandAttrs cards b depth ix) (f bet)
+
+phAttrsSplitDepth :: Lens' PlayerHandAttrs Int
+phAttrsSplitDepth f (PlayerHandAttrs cards bet depth ix) = fmap (\d -> PlayerHandAttrs cards bet d ix) (f depth)
+
+phAttrsHandIx :: Lens' PlayerHandAttrs Int
+phAttrsHandIx f (PlayerHandAttrs cards bet depth ix) = fmap (PlayerHandAttrs cards bet depth) (f ix)
+
+-- PlayerHandModes
+phFsm :: Lens' PlayerHandModes SomePlayerHandFSM
+phFsm f (PlayerHandModes fsm) = fmap PlayerHandModes (f fsm)
+
+-- PlayerHandRels
+phRelsBelongsToPlayerSpot :: Lens' PlayerHandRels (EntityId 'PlayerSpot)
+phRelsBelongsToPlayerSpot f (PlayerHandRels spot round player) = fmap (\s -> PlayerHandRels s round player) (f spot)
+
+phRelsBelongsToDealerRound :: Lens' PlayerHandRels (EntityId 'DealerRound)
+phRelsBelongsToDealerRound f (PlayerHandRels spot round player) = fmap (\r -> PlayerHandRels spot r player) (f round)
+
+phRelsOwnedByPlayer :: Lens' PlayerHandRels (EntityId 'Player)
+phRelsOwnedByPlayer f (PlayerHandRels spot round player) = fmap (PlayerHandRels spot round) (f player)
+
+-- PlayerSpotAttrs
+psAttrsSpotIndex :: Lens' PlayerSpotAttrs PlayerSpotIx
+psAttrsSpotIndex f (PlayerSpotAttrs ix wager) = fmap (`PlayerSpotAttrs` wager) (f ix)
+
+psAttrsWager :: Lens' PlayerSpotAttrs Chips
+psAttrsWager f (PlayerSpotAttrs ix wager) = fmap (PlayerSpotAttrs ix) (f wager)
+
+-- PlayerSpotModes
+psModesPlayerSpot :: Lens' PlayerSpotModes SomePlayerSpotFSM
+psModesPlayerSpot f (PlayerSpotModes fsm) = fmap PlayerSpotModes (f fsm)
+
+-- PlayerSpotRels
+psEntityRelsPlayerId :: Lens' PlayerSpotRels (EntityId 'Player)
+psEntityRelsPlayerId f (PlayerSpotRels player round occupancy) = fmap (\p -> PlayerSpotRels p round occupancy) (f player)
+
+psEntityRelsRoundId :: Lens' PlayerSpotRels (EntityId 'DealerRound)
+psEntityRelsRoundId f (PlayerSpotRels player round occupancy) = fmap (\r -> PlayerSpotRels player r occupancy) (f round)
+
+psRelsHandOccupancy :: Lens' PlayerSpotRels (FiniteMap PlayerSpotHandIx (Occupancy (EntityId 'PlayerHand)))
+psRelsHandOccupancy f (PlayerSpotRels player round occupancy) = fmap (PlayerSpotRels player round) (f occupancy)
+
+-- TableAttrs
+tAttrsName :: Lens' TableAttrs String
+tAttrsName f (TableAttrs name round offering minBet) = fmap (\n -> TableAttrs n round offering minBet) (f name)
+
+tAttrsCurrentRound :: Lens' TableAttrs (Maybe (EntityId 'DealerRound))
+tAttrsCurrentRound f (TableAttrs name round offering minBet) = fmap (\r -> TableAttrs name r offering minBet) (f round)
+
+tAttrsOfferingUsed :: Lens' TableAttrs (EntityId 'Offering)
+tAttrsOfferingUsed f (TableAttrs name round offering minBet) = fmap (\o -> TableAttrs name round o minBet) (f offering)
+
+tAttrsMinBet :: Lens' TableAttrs Chips
+tAttrsMinBet f (TableAttrs name round offering minBet) = fmap (TableAttrs name round offering) (f minBet)
+
+-- TableModes
+tModesFSM :: Lens' TableModes SomeTableFSM
+tModesFSM f (TableModes fsm) = fmap TableModes (f fsm)
+
+-- TableRels
+tRelsManagedByDealer :: Lens' TableRels (Maybe (EntityId 'Dealer))
+tRelsManagedByDealer f (TableRels dealer) = fmap TableRels (f dealer)
+
+-- TableShoeAttrs
+tsAttrsCards :: Lens' TableShoeAttrs [Card]
+tsAttrsCards f (TableShoeAttrs cards states) = fmap (`TableShoeAttrs` states) (f cards)
+
+tsAttrsCardStates :: Lens' TableShoeAttrs (Map CardIx CardState)
+tsAttrsCardStates f (TableShoeAttrs cards states) = fmap (TableShoeAttrs cards) (f states)
+
+-- TableShoeRels
+tsRelsTable :: Lens' TableShoeRels (EntityId 'Table)
+tsRelsTable f (TableShoeRels table) = fmap TableShoeRels (f table)
--- src/Pitboss/State/Types/Core.hs
+++ src/Pitboss/State/Types/Core.hs
@@ -6,32 +6,34 @@
 module Pitboss.State.Types.Core (
     EntityKind (..),
     EntityStatePart (..),
-    EntityStateSelector (..),
+    DeltaSemantics (..),
+    EntityId (..),
     Uid (..),
     UidPrefix,
     EntityRef (..),
     Tick (..),
+    uidTick,
+    uidEntityId,
+    entityRefTick,
+    entityRefId,
+    generateUid,
     displayUid,
     parseDisplayUid,
-    generateUid,
-    uidToWord64,
 ) where
 
-import Data.Hashable (Hashable (..))
-import Data.Word (Word64)
-
-import Data.Aeson (FromJSON (..), FromJSONKey, KeyValue ((.=)), ToJSON (..), ToJSONKey, object, withObject, withText, (.:))
+import Data.Aeson (FromJSON (..), FromJSONKey, ToJSON (..), ToJSONKey, withText)
 import Data.Bits (Bits ((.|.)), shiftL)
 import Data.Char (toUpper)
 import Data.Data (Proxy (..))
+import Data.Hashable (Hashable (..))
 import Data.Map.Strict (Map)
 import Data.Map.Strict qualified as Map
-import Data.Maybe (mapMaybe)
 import Data.Text qualified as T
+import Data.Word (Word64)
 import GHC.Generics (Generic)
 import GHC.TypeLits (KnownSymbol, Symbol, symbolVal)
 import Numeric (showIntAtBase)
-import System.Random (Random (..), StdGen)
+import System.Random (Random (..), RandomGen)
 
 -- entity identification
 
@@ -50,82 +52,78 @@ data EntityKind
 data EntityStatePart = Attrs | Modes | Rels
     deriving (Eq, Show, Generic)
 
-data EntityStateSelector
-    = Whole
-    | Part EntityStatePart
-    deriving (Eq, Show, Generic)
+data DeltaSemantics
+    = TransactionBoundary -- "This delta marks semantic completion"
+    | PartialUpdate EntityStatePart -- "This delta modifies a specific part"
+
+-- time
+
+newtype Tick = Tick Word64
+    deriving (Eq, Ord, Show, Hashable, ToJSON, FromJSON, ToJSONKey, FromJSONKey)
 
 -- uid
 
-newtype Uid (k :: EntityKind) = Uid String
+newtype EntityId (k :: EntityKind) = EntityId Word64
     deriving (Eq, Ord, Show, Generic)
 
-instance forall k. (KnownSymbol (UidPrefix k)) => ToJSON (Uid k) where
-    toJSON = toJSON . displayUid
+newtype Uid (k :: EntityKind) = Uid (Tick, EntityId k)
+    deriving (Eq, Ord, Show, Generic)
 
-instance forall k. (KnownSymbol (UidPrefix k)) => FromJSON (Uid k) where
-    parseJSON = withText "Uid" $ \t ->
-        case parseDisplayUid (T.unpack t) of
-            Just uid -> pure uid
-            Nothing -> fail "Invalid prefixed UID format"
+newtype EntityRef (k :: EntityKind) = EntityRef (Uid k)
+    deriving (Eq, Ord, Show, Generic)
 
-instance Hashable (Uid k) where
-    hashWithSalt salt (Uid k) = hashWithSalt salt (uidToWord64 (Uid k))
+-- Helper functions
+uidTick :: Uid k -> Tick
+uidTick (Uid (tick, _)) = tick
 
-generateUid :: EntityKind -> Int -> StdGen -> (Uid k, StdGen)
-generateUid _ counter gen =
+uidEntityId :: Uid k -> EntityId k
+uidEntityId (Uid (_, entityId)) = entityId
+
+entityRefTick :: EntityRef k -> Tick
+entityRefTick (EntityRef uid) = uidTick uid
+
+entityRefId :: EntityRef k -> EntityId k
+entityRefId (EntityRef uid) = uidEntityId uid
+
+generateUid :: (RandomGen g) => p -> Tick -> g -> (Uid k, g)
+generateUid _ tick gen =
     let (entropy, gen') = randomR (0, 2 ^ (40 :: Int) - 1) gen
-        showBase32Digit :: Int -> Char
-        showBase32Digit n
-            | n >= 0 && n < length base32Chars = base32Chars !! n
-            | otherwise = error $ "Invalid base32 digit: " ++ show n
-          where
-            base32Chars = "0123456789ABCDEFGHJKMNPQRSTVWXYZ"
+     in (Uid (tick, EntityId entropy), gen')
 
-        padLeft :: Char -> Int -> String -> String
-        padLeft c width str' = replicate (width - length str') c ++ str
+-- String encoding/decoding
+encodeUid :: Tick -> EntityId k -> String
+encodeUid (Tick tickWord) (EntityId entropy) =
+    let tickInt = fromIntegral tickWord `mod` (32 ^ (6 :: Int))
+        tsPart = showPaddedBase32 tickInt 6
+        rndPart = showPaddedBase32 (fromIntegral entropy) 8
+     in tsPart ++ "-" ++ rndPart
 
-        showPaddedBase32 :: Int -> Int -> String
-        showPaddedBase32 n width = padLeft '0' width (showIntAtBase 32 showBase32Digit n "")
-
-        tsPart = showPaddedBase32 counter 6
-        rndPart = showPaddedBase32 entropy 8
-        str = tsPart ++ "-" ++ rndPart
-
-        uid' = if isValidUidFormat str then Just (Uid str) else Nothing
-     in case uid' of
-            Just uid -> (uid, gen')
-            Nothing -> error "Internal error: generated invalid Uid"
-
-parseUid :: forall k. (KnownSymbol (UidPrefix k)) => String -> Maybe (Uid k)
-parseUid str = do
-    let expectedPrefix = symbolVal (Proxy @(UidPrefix k))
+decodeUid :: String -> Maybe (Tick, EntityId k)
+decodeUid str =
     case break (== '-') str of
-        (actualPrefix, '-' : rest)
-            | actualPrefix == expectedPrefix ->
-                if isValidUidFormat rest
-                    then Just (Uid str)
-                    else Nothing
+        (prefix, '-' : suffix)
+            | length prefix == 6 && length suffix == 8
+            , all isBase32Char prefix && all isBase32Char suffix -> do
+                tickDigits <- mapM decodeBase32Char prefix
+                entropyDigits <- mapM decodeBase32Char suffix
+                let tickWord = foldl (\acc d -> acc `shiftL` 5 .|. fromIntegral d) 0 tickDigits
+                    entropy = foldl (\acc d -> acc `shiftL` 5 .|. fromIntegral d) 0 entropyDigits
+                pure (Tick tickWord, EntityId entropy)
         _ -> Nothing
 
-isValidUidFormat :: String -> Bool
-isValidUidFormat str' =
-    case break (== '-') str' of
-        (prefix, '-' : suffix) ->
-            length prefix == 6
-                && length suffix == 8
-                && all isBase32Char prefix
-                && all isBase32Char suffix
-        _ -> False
+-- Base32 helpers
+showPaddedBase32 :: Int -> Int -> String
+showPaddedBase32 n width =
+    let base32Chars = "0123456789ABCDEFGHJKMNPQRSTVWXYZ"
+        showBase32Digit i
+            | i >= 0 && i < length base32Chars = base32Chars !! i
+            | otherwise = error $ "Invalid base32 digit: " ++ show i
+        result = showIntAtBase 32 showBase32Digit n ""
+        padLeft c w s = replicate (w - length s) c ++ s
+     in padLeft '0' width result
 
-uidToWord64 :: Uid k -> Word64
-uidToWord64 (Uid s) =
-    let
-     in case Prelude.break (== '-') s of
-            (prefix, '-' : suffix) ->
-                let digits = mapMaybe decodeBase32Char (prefix ++ suffix)
-                 in foldl (\acc d -> acc `shiftL` 5 .|. fromIntegral d) 0 digits
-            _ -> error $ "Invalid Uid: " ++ s
+isBase32Char :: Char -> Bool
+isBase32Char c = toUpper c `elem` ("0123456789ABCDEFGHJKMNPQRSTVWXYZ" :: String)
 
 decodeBase32Char :: Char -> Maybe Int
 decodeBase32Char = (`Map.lookup` base32Map) . toUpper
@@ -145,38 +143,48 @@ type family UidPrefix (k :: EntityKind) :: Symbol where
     UidPrefix 'TableShoe = "SHO"
 
 displayUid :: forall k. (KnownSymbol (UidPrefix k)) => Uid k -> String
-displayUid (Uid core) =
+displayUid (Uid (tick, entityId)) =
     let prefix = symbolVal (Proxy @(UidPrefix k))
-     in prefix ++ "-" ++ core
+        encoded = encodeUid tick entityId
+     in prefix ++ "-" ++ encoded
 
 parseDisplayUid :: forall k. (KnownSymbol (UidPrefix k)) => String -> Maybe (Uid k)
 parseDisplayUid str = do
     let expectedPrefix = symbolVal (Proxy @(UidPrefix k))
     case break (== '-') str of
-        (actualPrefix, '-' : core)
-            | actualPrefix == expectedPrefix -> parseUid @k core
+        (actualPrefix, '-' : rest)
+            | actualPrefix == expectedPrefix -> do
+                (tick, entityId) <- decodeUid rest
+                pure (Uid (tick, entityId))
         _ -> Nothing
 
-isBase32Char :: Char -> Bool
-isBase32Char c = toUpper c `elem` ("0123456789ABCDEFGHJKMNPQRSTVWXYZ" :: String)
+instance forall k. (KnownSymbol (UidPrefix k)) => ToJSON (Uid k) where
+    toJSON = toJSON . displayUid
 
--- time
+instance forall k. (KnownSymbol (UidPrefix k)) => FromJSON (Uid k) where
+    parseJSON = withText "Uid" $ \t ->
+        case parseDisplayUid (T.unpack t) of
+            Just uid -> pure uid
+            Nothing -> fail "Invalid prefixed UID format"
 
-newtype Tick = Tick Word64
-    deriving (Eq, Ord, Show, Hashable, ToJSON, FromJSON, ToJSONKey, FromJSONKey)
+instance ToJSON (EntityId k) where
+    toJSON (EntityId entropy) = toJSON entropy
 
-data Clocked a = Clocked' Tick a
-    deriving (Eq, Ord, Show, Generic)
+instance FromJSON (EntityId k) where
+    parseJSON v = EntityId <$> parseJSON v
 
-newtype EntityRef (k :: EntityKind) = Clocked (Uid k)
-    deriving (Eq, Ord, Show, Generic)
+instance forall k. (KnownSymbol (UidPrefix k)) => ToJSON (EntityRef k) where
+    toJSON (EntityRef uid) = toJSON uid
 
-instance (ToJSON (EntityRef k), ToJSON (Uid k)) => ToJSON (EntityRef k) where
-    toJSON (Clocked x) = object ["variant" .= ("Clocked" :: String), "value" .= x]
+instance forall k. (KnownSymbol (UidPrefix k)) => FromJSON (EntityRef k) where
+    parseJSON v = EntityRef <$> parseJSON v
 
-instance (FromJSON (EntityRef k), FromJSON (Uid k)) => FromJSON (EntityRef k) where
-    parseJSON = withObject "EntityRef" $ \o -> do
-        variant <- o .: "variant"
-        case variant :: String of
-            "Clocked" -> Clocked <$> o .: "value"
-            _ -> fail ("Unknown EntityRef variant: " ++ variant)
+instance Hashable (EntityId k) where
+    hashWithSalt salt (EntityId entropy) = hashWithSalt salt entropy
+
+instance Hashable (Uid k) where
+    hashWithSalt salt (Uid (Tick tick, entityId)) =
+        hashWithSalt (hashWithSalt salt tick) entityId
+
+instance Hashable (EntityRef k) where
+    hashWithSalt salt (EntityRef uid) = hashWithSalt salt uid

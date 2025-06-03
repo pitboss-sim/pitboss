23bf1fb10d1cbebb21b1be3dd5cac85fa105ec70
Pitboss <pitboss.sim@gmail.com>
2025-06-02 18:48:02 -0700

    blackjack types

---
+++ src/Pitboss/Blackjack/Offering/RuleSet.hs
@@ -0,0 +1,7 @@
+module Pitboss.Blackjack.Offering.RuleSet (
+    module Pitboss.Blackjack.Offering.RuleSet.Game,
+    module Pitboss.Blackjack.Offering.RuleSet.Table,
+) where
+
+import Pitboss.Blackjack.Offering.RuleSet.Game
+import Pitboss.Blackjack.Offering.RuleSet.Table

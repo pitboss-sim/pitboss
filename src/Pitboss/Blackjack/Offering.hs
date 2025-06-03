6c5df04e54959a7148b87e9d55fa5e2fac144cc2
Pitboss <pitboss.sim@gmail.com>
2025-06-02 17:41:23 -0700

    fixup blackjack

--- src/Pitboss/Blackjack/Offering.hs
+++ src/Pitboss/Blackjack/Offering.hs
@@ -1,5 +1,5 @@
 module Pitboss.Blackjack.Offering (
-    module Pitboss.Blackjack.Offering.Matter,
+    module Pitboss.Blackjack.Offering.Materia,
     module Pitboss.Blackjack.Offering.RuleSet,
     Offering (..),
     mkOffering,
@@ -7,17 +7,18 @@ module Pitboss.Blackjack.Offering (
 
 import Data.Aeson (FromJSON, ToJSON)
 import GHC.Generics (Generic)
-import Pitboss.Blackjack.Offering.Matter
+import Pitboss.Blackjack.Offering.Materia
 import Pitboss.Blackjack.Offering.RuleSet
 
 data Offering = Offering
-    { matter :: Matter
-    , ruleSet :: RuleSet
+    { materia :: Materia
+    , gameRuleSet :: GameRuleSet
+    , tableRuleSet :: TableRuleSet
     }
     deriving (Eq, Show, Generic)
 
 instance ToJSON Offering
 instance FromJSON Offering
 
-mkOffering :: Matter -> RuleSet -> Offering
-mkOffering m r = Offering{matter = m, ruleSet = r}
+mkOffering :: Materia -> GameRuleSet -> TableRuleSet -> Offering
+mkOffering m g t = Offering{materia = m, gameRuleSet = g, tableRuleSet = t}
---
+++ src/Pitboss/Blackjack/Outcome.hs
@@ -0,0 +1,55 @@
+{-# LANGUAGE DeriveGeneric #-}
+
+module Pitboss.Blackjack.Outcome where
+
+import Data.Aeson (FromJSON, ToJSON)
+import GHC.Generics (Generic)
+
+data BoutOutcome
+    = PlayerWins
+    | DealerWins
+    | Push
+    deriving (Eq, Show, Generic)
+
+data WinReason
+    = HigherScore
+    | OpponentBust
+    | NaturalBlackjack
+    deriving (Eq, Show, Generic)
+
+data DetailedOutcome = DetailedOutcome
+    { outcome :: BoutOutcome
+    , reason :: Maybe WinReason
+    }
+    deriving (Eq, Show, Generic)
+
+instance ToJSON BoutOutcome
+instance FromJSON BoutOutcome
+instance ToJSON WinReason
+instance FromJSON WinReason
+instance ToJSON DetailedOutcome
+instance FromJSON DetailedOutcome
+
+playerWinsHigher :: DetailedOutcome
+playerWinsHigher = DetailedOutcome PlayerWins (Just HigherScore)
+
+playerWinsDealerBust :: DetailedOutcome
+playerWinsDealerBust = DetailedOutcome PlayerWins (Just OpponentBust)
+
+playerWinsBlackjack :: DetailedOutcome
+playerWinsBlackjack = DetailedOutcome PlayerWins (Just NaturalBlackjack)
+
+dealerWinsHigher :: DetailedOutcome
+dealerWinsHigher = DetailedOutcome DealerWins (Just HigherScore)
+
+dealerWinsPlayerBust :: DetailedOutcome
+dealerWinsPlayerBust = DetailedOutcome DealerWins (Just OpponentBust)
+
+dealerWinsBlackjack :: DetailedOutcome
+dealerWinsBlackjack = DetailedOutcome DealerWins (Just NaturalBlackjack)
+
+pushOutcome :: DetailedOutcome
+pushOutcome = DetailedOutcome Push Nothing
+
+basicOutcome :: DetailedOutcome -> BoutOutcome
+basicOutcome = outcome

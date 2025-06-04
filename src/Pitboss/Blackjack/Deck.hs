49607caeed2a5b65686000081f98200cb4ca9552
Pitboss <pitboss.sim@gmail.com>
2025-06-03 17:58:54 -0700

    step 3

--- src/Pitboss/Blackjack/Deck.hs
+++ src/Pitboss/Blackjack/Deck.hs
@@ -2,7 +2,7 @@ module Pitboss.Blackjack.Deck where
 
 import Control.Monad.ST (ST, runST)
 import Data.Array.ST (STArray, getElems, newListArray, readArray, writeArray)
-import Pitboss.Blackjack.Offering.Materia
+import Pitboss.Blackjack.Rules.Offering
 import Pitboss.Blackjack.Types.Core
 import System.Random (StdGen, randomR)
 

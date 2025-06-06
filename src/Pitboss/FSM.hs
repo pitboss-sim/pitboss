bdc37a0bc1a0a56213cc564c3024c579d2355cc9
Pitboss <pitboss.sim@gmail.com>
2025-06-05 23:40:56 -0700

    finite state machines: facade

---
+++ src/Pitboss/FSM.hs
@@ -0,0 +1,34 @@
+{- | Public facade for all Pitboss FSM types.
+
+This module re-exports all FSM modules for convenient access.
+-}
+module Pitboss.FSM (
+    -- * Player State Machines
+    module Pitboss.FSM.Player.Hand,
+    module Pitboss.FSM.Player.Spot,
+    module Pitboss.FSM.Player.Table,
+
+    -- * Dealer State Machines
+    module Pitboss.FSM.Dealer.Hand,
+    module Pitboss.FSM.Dealer.Round,
+    module Pitboss.FSM.Dealer.Table,
+
+    -- * Game State Machines
+    module Pitboss.FSM.Bout,
+    module Pitboss.FSM.Table,
+
+    -- * Common Types
+    module Pitboss.FSM.Transitionable,
+    module Pitboss.FSM.Types,
+) where
+
+import Pitboss.FSM.Bout
+import Pitboss.FSM.Dealer.Hand
+import Pitboss.FSM.Dealer.Round
+import Pitboss.FSM.Dealer.Table
+import Pitboss.FSM.Player.Hand
+import Pitboss.FSM.Player.Spot
+import Pitboss.FSM.Player.Table
+import Pitboss.FSM.Table
+import Pitboss.FSM.Transitionable
+import Pitboss.FSM.Types

8f4f9bf386d47990b1f799e7d5beaa9bf589a3d7
Pitboss <pitboss.sim@gmail.com>
2025-06-06 22:36:23 -0700

    fixup sim:engine

--- src/Pitboss/Simulation/Types.hs
+++ src/Pitboss/Simulation/Types.hs
@@ -6,6 +6,8 @@ import Data.HashMap.Strict.InsOrd
 import Data.Word (Word64)
 import Pitboss.Causality
 import Pitboss.Simulation.Event
+import GHC.Generics (Generic)
+import Data.Aeson (ToJSON, FromJSON)
 
 data SimState = SimState
     { simTrace :: Trace
@@ -17,10 +19,18 @@ data SimState = SimState
 newtype EventLog = EventLog
     { eventLogEvents :: InsOrdHashMap Tick [SimEvent]
     }
+    deriving (Eq, Show, Generic)
+
+instance ToJSON EventLog
+instance FromJSON EventLog
 
 newtype IntentLog = IntentLog
     { intentLogIntents :: InsOrdHashMap Tick [SimIntent]
     }
+    deriving (Eq, Show, Generic)
+
+instance ToJSON IntentLog
+instance FromJSON IntentLog
 
 data SimIntent = SimIntent
     { intentId :: IntentId
@@ -28,8 +38,10 @@ data SimIntent = SimIntent
     , intentDesiredEvent :: BlackjackEvent
     , intentTimestamp :: Tick
     }
+    deriving (Eq, Show, Generic)
 
-deriving instance Show SimIntent
+instance ToJSON SimIntent
+instance FromJSON SimIntent
 
 data SimEvent = SimEvent
     { eventId :: EventId
@@ -37,25 +49,38 @@ data SimEvent = SimEvent
     , eventTimestamp :: Tick
     , eventCausingIntent :: Maybe Word64
     }
+    deriving (Eq, Show, Generic)
 
-deriving instance Show SimEvent
+instance ToJSON SimEvent
+instance FromJSON SimEvent
 
 data SimLogEntry
     = IntentLogged SimIntent
     | IntentValidated Word64 ValidationOutcome
     | EventLogged SimEvent
     | TraceOp TraceOp
+    deriving (Eq, Show, Generic)
 
-deriving instance Show SimLogEntry
+instance ToJSON SimLogEntry
+instance FromJSON SimLogEntry
 
 data ValidationOutcome = Accepted | Rejected RejectionReason
-    deriving (Show)
+    deriving (Eq, Show, Generic)
+
+instance ToJSON ValidationOutcome
+instance FromJSON ValidationOutcome
 
 data RejectionReason = NotYourTurn | InvalidAction | InsufficientFunds
-    deriving (Show)
+    deriving (Eq, Show, Generic)
+
+instance ToJSON RejectionReason
+instance FromJSON RejectionReason
 
 data ActorId
     = ActorPlayer (EntityId 'Player)
     | ActorDealer (EntityId 'Dealer)
     | ActorTable (EntityId 'Table)
-    deriving (Show)
+    deriving (Eq, Show, Generic)
+
+instance ToJSON ActorId
+instance FromJSON ActorId
